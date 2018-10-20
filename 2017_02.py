54c62c1d7c20bcde8790ade381c5921fd6299a43
Dawid Płocki <dawid.plocki@gmail.com>
2018-10-20 19:17:55 +0200

    Solution for second part of second task in 2017

--- 2017_02.py
+++ 2017_02.py
@@ -1,9 +1,24 @@
+def find_pair(row):
+    sorted_row = sorted(row, reverse=True)
+
+    while sorted_row:
+        the_biggest = sorted_row.pop(0)
+        for x in sorted_row:
+            if the_biggest % x == 0:
+                return (the_biggest, x)
+
+
 input_array = [
     # '\t+' -> ', '
     # '^' -> '['
     # '$' -> '],'
 ]
 
-result = sum([max(row) - min(row) for row in input_array])
+# First part
+result_a = sum([max(row) - min(row) for row in input_array])
+print(result_a)
 
-print(result)
+# Second part
+pairs_table = [find_pair(row) for row in input_array]
+result_b = sum([int(x / y) for x, y in pairs_table])
+print(result_b)
