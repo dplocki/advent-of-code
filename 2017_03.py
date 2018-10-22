7e2dd06cb4800a0c082a9f89ad74a0facaa5dd25
Dawid Płocki <dawid.plocki@gmail.com>
2018-10-22 21:08:40 +0200

    Solution for third task (second part) in 2017

--- 2017_03.py
+++ 2017_03.py
@@ -27,7 +27,7 @@ def steps_generator():
         for _ in range(steps):
             x += 1
             yield (x, y)
-   
+
 
 def calculate_steps(cell_number):
     generator = steps_generator()
@@ -38,20 +38,43 @@ def calculate_steps(cell_number):
 
     return abs(result[0]) + abs(result[1])
 
-def test(cell_number, excepted):
+def test_a(cell_number, excepted):
     result = calculate_steps(cell_number)
     assert result == excepted, f"For {cell_number} Excepted: {excepted} Recived: {result}"
 
 
-test(1, 0)
+test_a(1, 0)
 
-test(2, 1)
-test(3, 2)
-test(4, 1)
-test(5, 2)
+test_a(2, 1)
+test_a(3, 2)
+test_a(4, 1)
+test_a(5, 2)
 
-test(12, 3)
-test(23, 2)
-test(1024, 31)
+test_a(12, 3)
+test_a(23, 2)
+test_a(1024, 31)
 
-print(calculate_steps(312051))
+
+def find_first_written_value_bigger_than(number):
+    memory = {(0,0): 1}
+
+    generator = steps_generator()
+    next(generator)  # skip one
+
+    while True:
+        coordinates = next(generator)
+        result = 0
+
+        for x, y in [(x, y) for x in [-1, 0, 1] for y in [-1, 0, 1] if not (x == y == 0)]:
+            result += memory.get((coordinates[0] + x, coordinates[1] + y), 0)
+    
+        memory[coordinates] = result
+
+        if result > number:
+            return result
+
+# First part
+print("Solution of first part: ", calculate_steps(312051))
+
+# Second part
+print("Solution of second part: ", find_first_written_value_bigger_than(312051))
