79125b81d35a08b884be0e453eb599d1214a994b
Dawid Płocki <dawid.plocki@gmail.com>
2018-10-27 20:07:16 +0200

    Solution for 9th task (second part) in 2017

--- 2017_09.py
+++ 2017_09.py
@@ -1,43 +1,56 @@
 import re
 
+def first_part(input_content):
 
-def clean_up(input: str):
-    return re.sub(r'<.*?>', '', re.sub(r'!.', '', input))
+    def clean_up(input: str):
+        return re.sub(r'<.*?>', '', re.sub(r'!.', '', input))
 
 
-def calculate(input: str):
-    input = clean_up(input)
+    def calculate(input: str):
+        input = clean_up(input)
 
-    score = 0
+        score = 0
+        result = 0
+
+        for char in input:
+            if char == '{':
+                score += 1
+                result += score
+            elif char == '}':
+                score -= 1
+
+        return result
+
+
+    def test(expected: int, input: str):
+        result = calculate(input)
+        assert result == expected, f"Expected: {expected} get: {result}"
+
+
+    if input == None:
+        test(1, '{}')
+        test(6, '{{{}}}')
+        test(5, '{{},{}}')
+        test(16, '{{{},{},{{}}}}')
+        test(1, '{<a>,<a>,<a>,<a>}')
+        test(9, '{{<a>},{<a>},{<a>},{<a>}}')
+        test(9, '{{<!!>},{<!!>},{<!!>},{<!!>}}')
+        test(3, '{{<a!>},{<a!>},{<a!>},{<ab>}}')
+    else:
+        result = calculate(input_content)
+        print(f'Solution: {result}')
+
+
+def second_part(input_content):
     result = 0
+    input = re.sub(r'!.', '', input_content)
+    for m in re.finditer(r'<.*?>', input):
+        result += len(m.group(0)) - 2
 
-    for char in input:
-        if char == '{':
-            score += 1
-            result += score
-        elif char == '}':
-            score -= 1
+    print(f"Solution: {result}")
 
-    return result
-
-
-def test(expected: int, input: str):
-    result = calculate(input)
-    assert result == expected, f"Expected: {expected} get: {result}"
-
-
-test(1, '{}')
-test(6, '{{{}}}')
-test(5, '{{},{}}')
-test(16, '{{{},{},{{}}}}')
-test(1, '{<a>,<a>,<a>,<a>}')
-test(9, '{{<a>},{<a>},{<a>},{<a>}}')
-test(9, '{{<!!>},{<!!>},{<!!>},{<!!>}}')
-test(3, '{{<a!>},{<a!>},{<a!>},{<ab>}}')
-
-
+# Taken from https://adventofcode.com/2017/day/9/input
 with open('input.txt', 'r') as content_file:
     content = content_file.read()
-    result = calculate(content)
-
-    print(f'Solution: {result}')
+    first_part(content)
+    second_part(content)
