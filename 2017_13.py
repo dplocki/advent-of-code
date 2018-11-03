0a74006356b922aaa40e17237d6c50ef22d76b8b
Dawid Płocki <dawid.plocki@gmail.com>
2018-11-03 21:34:17 +0100

    Solution for 13th task (second part) in 2017

--- 2017_13.py
+++ 2017_13.py
@@ -1,63 +1,58 @@
-class NonScanner:
-    def __init__(self):
-        self.index = None
+def calculate_scanner_position(layer_lenght, time):
+    position_table_length = layer_lenght * 2 - 2
+    index_of_position = time % position_table_length
 
-    def move(self):
-        pass
+    if index_of_position < layer_lenght:
+        return index_of_position
 
-class Scanner:
-    def __init__(self, layer):
-        self.index = 0
-        self.layer = layer
-        self.direction = 'down'
-    
-    def move(self):
-        if self.direction == 'down':
-            self.index += 1
-        elif self.direction == 'up':
-            self.index -= 1
-
-        if self.index == (self.layer -1):
-            self.direction = 'up'
-        elif self.index == 0:
-            self.direction = 'down'
+    return layer_lenght * 2 - index_of_position - 2
 
 
-def test_of_scanner(layer, time_index, expected):
-    scanner = Scanner(layer)
-    for _ in range(time_index):
-        scanner.move()
-        
-    assert scanner.index == expected, f"Excepted: {expected}, recived: {scanner.index}"
+def test_of_calculate_scanner_position(layer_lenght, time, expected):
+    result = calculate_scanner_position(layer_lenght, time)
+    assert result == expected, f"Excepted: {expected}, recived: {result}"
 
-test_of_scanner(3, 0, 0)
-test_of_scanner(3, 1, 1)
-test_of_scanner(3, 2, 2)
-test_of_scanner(3, 3, 1)
-test_of_scanner(3, 4, 0)
-test_of_scanner(3, 5, 1)
-test_of_scanner(3, 6, 2)
-test_of_scanner(3, 7, 1)
-test_of_scanner(3, 8, 0)
 
-def simulate_walk_on_firewall(input):
-    the_lenght_of_input = max(input.keys()) + 1
-    firewall = {
-        index: Scanner(input[index]) if index in input else NonScanner()
-        for index in range(the_lenght_of_input)
-    }
+test_of_calculate_scanner_position(3, 0, 0)
+test_of_calculate_scanner_position(3, 1, 1)
+test_of_calculate_scanner_position(3, 2, 2)
+test_of_calculate_scanner_position(3, 3, 1)
+test_of_calculate_scanner_position(3, 4, 0)
+test_of_calculate_scanner_position(3, 5, 1)
+test_of_calculate_scanner_position(3, 6, 2)
+test_of_calculate_scanner_position(3, 7, 1)
+test_of_calculate_scanner_position(3, 8, 0)
 
+
+def find_scanners_on_index_zero(input, from_time):
+    for k, v in input.items():
+        if calculate_scanner_position(v, from_time + k) == 0:
+            yield k, v
+
+
+def calculate_severity(input, from_time: int = 0):
     result = 0
-    for index in range(the_lenght_of_input):
-        current_scaner = firewall[index]
-        if current_scaner.index == 0:
-            result += current_scaner.layer * index
 
-        # move scanners
-        for _, v in firewall.items():
-            v.move()
+    for k, v in find_scanners_on_index_zero(input, from_time):
+        result += v * k
 
     return result
+
+
+def find_how_long_wait_for_clear_path(input: {}, time_from: int = 0):
+
+    def is_path_clear(input: {}, time):
+        for _, _ in find_scanners_on_index_zero(input, time):
+            return False
+
+        return True
+
+    result = time_from
+    while True:
+        if is_path_clear(input, result):
+            return result
+
+        result += 1
 
 
 test_input = {
@@ -67,10 +62,12 @@ test_input = {
     6: 4
 }
 
-assert simulate_walk_on_firewall(test_input) == 24
+assert calculate_severity(test_input) == 24
+assert calculate_severity(test_input, 10) == 0
 
 input = {
     # get the content of https://adventofcode.com/2017/day/13/input and add the comas on end lines
 }
 
-print("Solution for first part: ", simulate_walk_on_firewall(input))
+print("Solution for the first part:", calculate_severity(input))
+print("Solution for the second part:", find_how_long_wait_for_clear_path(input))
