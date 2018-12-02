b4a2fc4c5682f153dc66c212d987ecd17010dd3d
Dawid Płocki <dawid.plocki@gmail.com>
2018-12-02 09:59:00 +0100

    Solution for second task (second part) in 2018

--- 2018_02.py
+++ 2018_02.py
@@ -1,10 +1,11 @@
 from collections import Counter
+import itertools
 
 
 def file_to_input_list(file_name):
     with open(file_name) as file:
         for line in file:
-            yield line
+            yield line.strip()
 
 
 def find_twice_and_threes(input):
@@ -40,3 +41,30 @@ assert count_twice_and_threes_for_list(test_input) == (4, 3)
 
 result = count_twice_and_threes_for_list(file_to_input_list('input.txt'))
 print('Solution for first part:', result[0] * result[1])
+
+
+def hamming_distance(input_a, input_b):
+    return sum(a != b for a, b in zip(input_a, input_b))
+
+
+def find_hamming_distance_equal_1(list):
+    for a, b in itertools.combinations(list, 2):
+        if hamming_distance(a, b) == 1:
+            return a, b
+
+
+second_part_test_input = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']
+
+second_part_test_result = find_hamming_distance_equal_1(second_part_test_input)
+assert second_part_test_result == ('fghij', 'fguij')
+
+
+def strip_difference_from_strings(input_a, input_b):
+    return ''.join(a for a, b in zip(input_a, input_b) if a == b)
+
+
+assert strip_difference_from_strings(*second_part_test_result) == 'fgij'
+
+
+result = strip_difference_from_strings(*find_hamming_distance_equal_1(file_to_input_list('input.txt')))
+print('Solution for second part:', result)
