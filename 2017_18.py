2b49a732ce0ce11476ed192ae587ece1e1acffa8
Dawid Płocki <dawid.plocki@gmail.com>
2018-12-08 15:09:17 +0100

    Solution for 18th task (second part) in 2017

--- 2017_18.py
+++ 2017_18.py
@@ -1,41 +1,39 @@
 # pylint: disable=E1111
 import re
 import sys
+from collections import deque
 
 
 class Computer():
 
     def __init__(self):
-        self.memory = {}
-        self.index = 0
+        self._memory = {}
+        self._index = 0
 
     def run_command(self, command, x, p):
         method = getattr(self, command)
 
         index_offset = method(x, p)
-        self.index += 1 if index_offset is None else index_offset
+        self._index += (index_offset if index_offset is not None else 1)
 
     def set(self, x, p):
-        self.memory[x] = self._register_or_number(p)
+        self._memory[x] = self._register_or_number(p)
 
     def add(self, x, p):
-        self.memory[x] = self.memory.get(x, 0) + self._register_or_number(p)
+        self._memory[x] = self._register_or_number(x) + self._register_or_number(p)
 
     def mul(self, x, p):
-        self.memory[x] = self.memory.get(x, 0) * self._register_or_number(p)
+        self._memory[x] = self._register_or_number(x) * self._register_or_number(p)
 
     def mod(self, x, p):
-        self.memory[x] = self.memory.get(x, 0) % self._register_or_number(p)
+        self._memory[x] = self._register_or_number(x) % self._register_or_number(p)
 
     def jgz(self, x, p):
         if self._register_or_number(x) > 0:
             return self._register_or_number(p)
 
     def _register_or_number(self, p) -> int:
-        if p.isalpha():
-            return self.memory.get(p, 0)
-        else:
-            return int(p)
+        return self._memory.get(p, 0) if p.isalpha() else int(p)
 
 
 class FirstPartComputer(Computer):
@@ -45,57 +43,60 @@ class FirstPartComputer(Computer):
         self.last_play_frequency = None
 
     def rcv(self, x, p):
-        if self.memory.get(x, 0) > 0:
+        if self._memory.get(x, 0) > 0:
             print("Solution for first part:", self.last_play_frequency)
             return sys.maxsize
 
     def snd(self, x, p):
-        self.last_play_frequency = self.memory.get(x, 0)
-
+        self.last_play_frequency = self._memory.get(x, 0)
 
     def run_program(self, program: []):
         program_len = len(program)
 
-        while self.index < program_len:
-            command = program[self.index]
+        while self._index < program_len:
+            command = program[self._index]
 
             self.run_command(*command)
 
 
 class SecondPartComputer(Computer):
 
-    def __init__(self, program_id):
+    def __init__(self, program_id, program):
         Computer.__init__(self)
-        self.to_send = []
-        self.recived = None
+
+        self.recived = deque()
         self.count_send = 0
         self.program_id = program_id
-        self.memory['p'] = program_id
-        self.last_command = None
+        self._memory['p'] = program_id
+        self.other_computer = None
+        self.program = program
+        self.program_length = len(program)
+        self.terminated = False
+        self.blocked = False
 
-    def rcv(self, x, p):
-        self.memory[x] = self.recived
+    def run_program(self):
+        self.terminated = self._index < 0 or self._index >= self.program_length
+        if self.terminated:
+            return
 
-    def snd(self, x, p):
-        self.count_send += 1
-        self.to_send.insert(0, self._register_or_number(x))
+        command, first_paramater, second_paramater = self.program[self._index]
+        if command == 'rcv':
+            if self.recived:
+                self._memory[first_paramater] = self.recived.popleft()
+                self._index += 1
+            else:
+                self.blocked = True
+                return
+        elif command == 'snd':
+            self.count_send += 1
+            self.other_computer.recived.append(self._register_or_number(first_paramater))
+            self.other_computer.blocked = False
+            self._index += 1
+        else:
+            self.run_command(command, first_paramater, second_paramater)
 
-    def run_program(self, program: []):
-        program_len = len(program)
-
-        while self.index < program_len:
-            command = program[self.index]
-            #print(f'[{self.program_id}] {self.index} ', *command)
-            self.last_command = command[0]
-
-            if self.last_command == 'rcv':
-                yield self.last_command
-
-            self.run_command(*command)
-
-            if self.last_command == 'snd':
-                yield self.last_command
-
+    def is_working(self):
+        return not self.terminated and not self.blocked        
 
 def load_file(file_name):
     with open(file_name) as file:
@@ -105,62 +106,31 @@ def load_file(file_name):
 
 def parse_lines(inputs: []):
     pattern = re.compile(r'^([a-z]{3}) ([0-9a-z])( ([\-0-9a-z]+))?')
-
     for line in inputs:
         group = pattern.match(line)
         yield (group[1], group[2], group[4])
 
 
 def run_parallel_programs(commands):
-    computer_0 = SecondPartComputer(0)
-    computer_0.generator = computer_0.run_program(commands)
+    computer_0 = SecondPartComputer(0, commands)
+    computer_1 = SecondPartComputer(1, commands)
 
-    computer_1 = SecondPartComputer(1)
-    computer_1.generator = computer_1.run_program(commands)
+    computer_0.other_computer = computer_1
+    computer_1.other_computer = computer_0
 
-    active = computer_0
-    non_active = computer_1
-
-    while computer_0.last_command != 'rcv' or computer_1.last_command != 'rcv':
-        tmp = next(active.generator)
-
-        if tmp == 'rcv':
-            if non_active.to_send:
-                active.recived = non_active.to_send.pop()
-                continue
-
-        elif tmp != 'snd':
-            continue
-
-        active, non_active = non_active, active
+    while computer_0.is_working() or computer_1.is_working():
+        computer_1.run_program()
+        computer_0.run_program()
 
     return {
         computer_0.program_id: computer_0.count_send,
         computer_1.program_id: computer_1.count_send
     }
 
-
-# # Solution for the first part
+# Solution for the first part, the input take from https://adventofcode.com/2017/day/18/input
 commands = [_ for _ in parse_lines(load_file('input.18.txt'))]
-# computer = FirstPartComputer()
-# computer.run_program(commands)
+computer = FirstPartComputer()
+computer.run_program(commands)
 
-# Tests for second part
-test_commands = [_ for _ in parse_lines(['snd 1', 'snd 2', 'snd p', 'rcv a', 'rcv b', 'rcv c', 'rcv d'])]
-
-test_computer = SecondPartComputer(0)
-for a, b in zip(test_computer.run_program(test_commands), ['snd'] * 3 + ['rcv'] * 4):
-    assert a == b, f"Excepted {b}, recived {a}"
-
-assert test_computer.to_send == [0, 2, 1]
-
-test_computer = SecondPartComputer(1)
-for a, b in zip(test_computer.run_program(test_commands), ['snd'] * 3 + ['rcv'] * 4):
-    assert a == b, f"Excepted {b}, recived {a}"
-
-assert test_computer.to_send == [1, 2, 1]
-assert run_parallel_programs(test_commands) == {0: 3, 1: 3}
-
-test_commands_2 = [_ for _ in parse_lines(['snd 1', 'rcv a', 'snd 2', 'rcv b', 'snd p', 'rcv c', 'rcv d'])]
-
-print(run_parallel_programs(commands))
+result = run_parallel_programs(commands)
+print('Solution for second part:', result[1])
