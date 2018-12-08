327391b06bf26d5786bf9429c370184e516c2244
Dawid Płocki <dawid.plocki@gmail.com>
2018-12-08 10:26:34 +0100

    Solution for 8th task (second part) in 2018

--- 2018_08.py
+++ 2018_08.py
@@ -25,3 +25,37 @@ assert get_meta_data_sum(input_generator('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'))
 # The input taken from https://adventofcode.com/2018/day/8/input
 input = '<input>'
 print('Solution for first part:', get_meta_data_sum(input_generator(input)))
+
+
+def get_root_node(input: [int]) -> int:
+    result = 0
+    children = next(input)
+    metadata = next(input)
+
+    if children == 0:
+        for _ in range(metadata):
+            result += next(input)
+    else:
+        children_roots = [get_root_node(input) for _ in range(children)]
+
+        for _ in range(metadata):
+            meta = next(input) - 1
+
+            result += children_roots[meta] if meta < children else 0
+
+    return result
+
+
+def test_root_node(input: str, excepted):
+    result = get_root_node(input_generator(input))
+
+    assert result == excepted, f"Except: {excepted} recived: {result}"
+
+
+test_root_node('0 1 99', 99)
+test_root_node('0 3 10 11 12', 33)
+test_root_node('1 1 0 1 99 2', 0)
+test_root_node('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2', 66)
+
+
+print('Solution for second part:', get_root_node(input_generator(input)))
