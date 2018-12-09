571f31969984f9692dd5aefe1531bf692de92d61
Dawid Płocki <dawid.plocki@gmail.com>
2018-12-09 13:36:50 +0100

    Solution for 9th task (second part) in 2018

--- 2018_09.py
+++ 2018_09.py
@@ -1,55 +1,52 @@
+from collections import deque
+
+
 def board_generator():
-    board = [0]
+    board = deque([0])
     current_marble = 1
-    current_marble_index = 0
+    removed = None
 
     while True:
-        board_lenght = len(board)
-        removed = []
-
         if current_marble % 23 != 0:
-            current_marble_index = (current_marble_index + 1) % board_lenght + 1
-            if current_marble_index > board_lenght:
-                board.append(current_marble)
-            else:
-                board.insert(current_marble_index, current_marble)
+            board.rotate(-1)
+            board.append(current_marble)
+            removed = None
         else:
-            removed.append(current_marble)
-            removed.append(board[current_marble_index - 7])
-            del board[current_marble_index - 7]
+            board.rotate(7)
+            removed = board.pop()
+            board.rotate(-1)
 
-            current_marble_index = (current_marble_index - 7) % board_lenght
-
-        yield current_marble, board, removed
+        yield current_marble, removed
         current_marble += 1
 
 
-def test_of_board(generator, expected_current_marble, expected_board, expected_removed = []):
-    current_marble, board, removed = next(generator)
+def test_of_board(generator, expected_current_marble, expected_removed = None):
+    current_marble, removed = next(generator)
 
     assert current_marble == expected_current_marble, f"Recived: {current_marble} Expected: {expected_current_marble}"
-    assert board == expected_board, f"Recived: {board} Expected: {expected_board}"
     assert removed == expected_removed, f"Recived: {removed} Expected: {expected_removed}"
 
 
 turn = board_generator()
-test_of_board(turn, 1, [0, 1])
-test_of_board(turn, 2, [0, 2, 1])
-test_of_board(turn, 3, [0, 2, 1, 3])
-test_of_board(turn, 4, [0, 4, 2, 1, 3])
-test_of_board(turn, 5, [0, 4, 2, 5, 1, 3])
-test_of_board(turn, 6, [0, 4, 2, 5, 1, 6, 3])
+test_of_board(turn, 1)
+test_of_board(turn, 2)
+test_of_board(turn, 3)
+test_of_board(turn, 4)
+test_of_board(turn, 5)
+test_of_board(turn, 6)
 
 turn = board_generator()
 [_ for _ in zip(turn, range(21))]
-test_of_board(turn, 23, [0, 16, 8, 17, 4, 18, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15], [23, 9])
+test_of_board(turn, 23, 9)
 
 
 def play(player_count, last_marble):
     player_scores = [0] * player_count
     
-    for current_marble, _, removed in board_generator():
-        player_scores[current_marble % player_count] += sum(removed)
+    for current_marble, removed in board_generator():
+        if removed:
+            player_scores[current_marble % player_count] += removed + current_marble
+
         if current_marble == last_marble:
             return player_scores
 
@@ -68,3 +65,4 @@ test_play(21, 6111, 54718)
 test_play(30, 5807, 37305)
 
 print('Solution for first part:', max(play(424, 71144)))
+print('Solution for second part:', max(play(424, 71144 * 100)))
