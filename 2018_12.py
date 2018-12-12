a7b6e70ffc5e111e099cfe1024ae49c1098b8ab8
Dawid Płocki <dawid.plocki@gmail.com>
2018-12-12 19:15:49 +0100

    Solution for 12th task (second part) in 2018

--- 2018_12.py
+++ 2018_12.py
@@ -6,12 +6,12 @@ def input_begin_state(input: str) -> {}:
     return { i: p == '#'  for i, p in enumerate(input)}
 
 
-def run_20_generation(rules, begin_state):
+def run_generation(rules, begin_state):
     start_index = 0
     end_index = len(begin_state) - 1
     actual_state = begin_state
 
-    for _ in range(20):
+    while True:
         new_state = {}
 
         for index in range(start_index, end_index + 1):
@@ -22,8 +22,7 @@ def run_20_generation(rules, begin_state):
         end_index += 2
 
         actual_state = new_state
-
-    return actual_state
+        yield actual_state
 
 
 def calculate_sum_of_pots_numbers(state: {}) -> int:
@@ -33,8 +32,13 @@ def calculate_sum_of_pots_numbers(state: {}) -> int:
 def solution_for_first_part(unparsed_rules, begin_state):
     rules = parse_rules(unparsed_rules)
     begin_state = input_begin_state(begin_state)
-    generation_20th_state = run_20_generation(rules, begin_state)
 
+    generator = run_generation(rules, begin_state)
+
+    for _ in range(19):
+        next(generator)
+
+    generation_20th_state = next(generator)
     return calculate_sum_of_pots_numbers(generation_20th_state)
 
 
@@ -58,4 +62,41 @@ test_input_begin = '#..#.#..##......###...###'
 assert solution_for_first_part(test_input_rules, test_input_begin) == 325
 
 # The input is taken from: https://adventofcode.com/2018/day/12/input
-print("Solution for first part:", solution_for_first_part('''<rules_input>''', '<initial_state_input>'))
+unparsed_rules = '''<input>'''
+unparsed_begin_state = '<input>'
+
+print("Solution for first part:", solution_for_first_part(unparsed_rules, unparsed_begin_state))
+
+rules = parse_rules(unparsed_rules)
+begin_state = input_begin_state(unparsed_begin_state)
+
+# generation_generator = run_generation(rules, begin_state)
+# i = 1
+# while True:
+#     generation = next(generation_generator)
+#     temp = calculate_sum_of_pots_numbers(generation)
+#     if i % 1000 == 0:
+#         print(f"test({i}, {temp})")
+#     i += 1
+
+#     if i == 5001:
+#         break
+
+
+def calulate_1000_multiplication(for_number: int) -> int:
+    t = int(for_number / 1000)
+    return (t * 36 - 1) * 1000 + 458
+
+
+def test(generation_number, excepted_result):
+    result = calulate_1000_multiplication(generation_number)
+    assert result == excepted_result, f"{result} but excepted {excepted_result} for {generation_number}"
+
+
+test(1000, 35458)
+test(2000, 71458)
+test(3000, 107458)
+test(4000, 143458)
+test(5000, 179458)
+
+print("Solution for second part:", calulate_1000_multiplication(50000000000))
