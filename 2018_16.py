1d1d37313dce2df81ffa7f8123086fa9e03e655d
Dawid Płocki <dawid.plocki@gmail.com>
2018-12-16 11:05:56 +0100

    Solution for 16th task (second part) in 2018

--- 2018_16.py
+++ 2018_16.py
@@ -1,6 +1,8 @@
 import re
 
 
+OPT_CODES_NUMBER = 16
+REGISTER_NUMBER = 4
 OPCODE = 0
 A = 1
 B = 2
@@ -124,13 +126,14 @@ opcodes = {
 
 
 def checking_sample(registers_before, instruction, registers_after) -> int:
-    behave = 0
-    for _, opcode in opcodes.items():
+    matching = set()
+
+    for op_name, opcode in opcodes.items():
         recived_registers_after = opcode(registers_before, instruction)
         if recived_registers_after == registers_after:
-            behave +=1
+            matching.add(op_name)
     
-    return behave
+    return matching
 
 
 def parse_input_file(file_name):
@@ -147,14 +150,66 @@ def how_many_samples_match_3_or_more(samples: []):
 
     for before, instruction, after in samples:
         matching = checking_sample(before, instruction, after)
-        if matching >= 3:
+        if len(matching) >= 3:
             result += 1
 
     return result
 
 
-assert checking_sample([3, 2, 1, 1], [9, 2, 1, 2], [3, 2, 2, 1]) == 3
+def samples_to_opt_codes_candidates(samples: []):
+    number_to_ops = { i: set() for i in range(OPT_CODES_NUMBER) }
+
+    for before, instruction, after in samples:
+        opt_code = instruction[0]
+
+        matching = checking_sample(before, instruction, after)
+        number_to_ops[opt_code].update(matching)
+
+    return number_to_ops
+
+
+def translate_opt_codes(samples: []):
+    opt_code_candidates = samples_to_opt_codes_candidates(samples)
+    result = {}
+
+    while len(result) < OPT_CODES_NUMBER:
+        # Find all with only one candidates
+        singulars = [(opt_code, list(candidates)[0]) for opt_code, candidates in opt_code_candidates.items() if len(candidates) == 1]
+        
+        for new_recognise_optcode, recognise_name in singulars:
+            result[new_recognise_optcode] = recognise_name
+
+            # Remove those from others list
+            for unknown_optcode, _ in opt_code_candidates.items():
+                opt_code_candidates[unknown_optcode].discard(recognise_name)
+
+    return result
+
+
+def parse_program_file(file_name: str):
+    pattern = re.compile(r'(\d+) (\d+) (\d+) (\d+)')
+
+    with open(file_name) as file:
+        for line in file:
+            group = pattern.match(line.strip())
+            yield int(group[1]), int(group[2]), int(group[3]), int(group[4])
+
+
+def run_program(opt_codes_to_instruction, program: [str]):
+    registers = [0] * REGISTER_NUMBER
+
+    for instruction in program:
+        registers = opcodes[opt_codes_to_instruction[instruction[0]]](registers, instruction)
+
+    return registers
+
+
+assert len(checking_sample([3, 2, 1, 1], [9, 2, 1, 2], [3, 2, 2, 1])) == 3
 
 # Input taken from: https://adventofcode.com/2018/day/16/input (only first part)
 print("Solution for first part:", how_many_samples_match_3_or_more(parse_input_file('input.first_part.txt')))
 
+# Input taken from: https://adventofcode.com/2018/day/16/input (only second part)
+program = parse_program_file('input.second_part.txt')
+
+print("Solution for second part:", run_program(translate_opt_codes(parse_input_file('input.first_part.txt')), program)[0])
