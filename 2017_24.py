ff49c5c7234c868f686bf95fd3fcd2ca0b779c1c
Dawid Płocki <dawid.plocki@gmail.com>
2019-01-12 11:16:46 +0100

    Solution for 24th task (second part) in 2017

--- 2017_24.py
+++ 2017_24.py
@@ -1,52 +1,66 @@
+TESTING = False
+
+
 def file_to_input_list(file_name):
     with open(file_name) as file:
         for line in file:
             yield line.strip()
 
 
-def joints(lines: [str]) -> {int:set}:
-
-    def parse_input(lines: [str]):
-        for line in lines:
-            yield tuple(map(int, line.split('/')))
-
-    return [(x, y) for x, y in parse_input(lines)]
-
-
-def find_all_matching_joints(joints: [tuple], ending: int):
-    return [join for join in joints if join[0] == ending or join[1] == ending]
-
-
-def build_brigdes(joints: [tuple], path: [], ending: int):
-    for joint in find_all_matching_joints(joints, ending):
-   
-        path.append(joint)
-        yield path
-
-        joints.remove(joint)
-
-        yield from build_brigdes(
-            joints,
-            path,
-            joint[0] if joint[1] == ending else joint[1]
-        )
-
-        joints.append(joint)
-        path.pop()
-
-
 def cost_of(path):
     return sum([x + y for x, y in path])
 
 
-def solution_for_first_part(lines: [str]):
+def build_all_bidges(lines: [str]):
+
+
+    def find_all_matching_joints(joints: [tuple], ending: int):
+        return [join for join in joints if join[0] == ending or join[1] == ending]
+
+
+    def parse_joints(lines: [str]):
+        for line in lines:
+            yield tuple(map(int, line.split('/')))
+
+
+    def build_brigdes(joints: list, path: list, ending: int):
+        for joint in find_all_matching_joints(joints, ending):
+            path.append(joint)
+
+            yield path[:]
+
+            joints.remove(joint)
+
+            yield from build_brigdes(
+                joints,
+                path,
+                joint[0] if joint[1] == ending else joint[1]
+            )
+
+            joints.append(joint)
+            path.pop()
+
+
+    return list(build_brigdes(list(parse_joints(lines)), [], 0))
+
+
+def solution_for_first_part(brigdes: [[int]]):
+    return max([cost_of(brigde) for brigde in brigdes])
+
+
+def solution_for_second_part(brigdes: [[int]]):
+    the_longest_lenght = max([len(brigde) for brigde in brigdes])
+
     return max([
-        cost_of(path)
-        for path in build_brigdes(joints(lines), [], 0)
+        cost_of(bridge)
+        for bridge in brigdes
+        if len(bridge) == the_longest_lenght
     ])
 
 
-test_input = '''0/2
+if TESTING:
+
+    test_input = '''0/2
 2/2
 2/3
 3/4
@@ -55,6 +69,13 @@ test_input = '''0/2
 10/1
 9/10'''
 
-assert solution_for_first_part(test_input.splitlines()) == 31
+    test_bridges = build_all_bidges(test_input.splitlines())
+    assert solution_for_first_part(test_bridges) == 31
+    assert solution_for_second_part(test_bridges) == 19
 
-print("Solution for the first part:", solution_for_first_part(file_to_input_list('input.24.txt')))
+else:
+    
+    # The input taken from https://adventofcode.com/2017/day/24/input
+    bridges = build_all_bidges(file_to_input_list('input.24.txt'))
+    print("Solution for the first part:", solution_for_first_part(bridges))
+    print("Solution for the second part:", solution_for_second_part(bridges))
