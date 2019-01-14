356dfb00d25c025a7ee43f4ac6f61b0fb72b78e7
Dawid Płocki <dawid.plocki@gmail.com>
2019-01-14 19:57:51 +0100

    Solution for second task (second part) in 2015

--- 2015/2015_02.py
+++ 2015/2015_02.py
@@ -10,11 +10,22 @@ def parse(lines: [str]):
 
 
 def calculate_size(l, w, h):
-    return  2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, l*h)
+    tmp = l*w, w*h, h*l
+    return 2 * sum(tmp) + min(tmp)
+
+
+def calculate_ribon_length(l, w, h):
+    return l * w * h + 2 * min(l + w, w + h, l + h)
 
 
 # The input is taken: https://adventofcode.com/2015/day/2/input
 print("Solution for the first part:", sum([
         calculate_size(l, w, h)
+        for l, w, h in parse(load_input_file('input.02.txt'))
+    ]))
+
+
+print("Solution for the second part:", sum([
+        calculate_ribon_length(l, w, h)
         for l, w, h in parse(load_input_file('input.02.txt'))
     ]))
