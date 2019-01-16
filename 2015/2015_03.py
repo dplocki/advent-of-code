7aaf4351b52606c272c1f628086b57d7a623a818
Dawid Płocki <dawid.plocki@gmail.com>
2019-01-16 22:02:19 +0100

    Solution for third task (second part) in 2015

--- 2015/2015_03.py
+++ 2015/2015_03.py
@@ -1,26 +1,50 @@
+COORDINATE_HASH_MAP = {
+    '>': (1, 0),
+    '<': (-1, 0),
+    '^': (0, -1),
+    'v': (0, 1)
+}
+
+
 def load_input_file(file_name):
     with open(file_name) as file:
         return file.read()
 
 
-def solution_to_the_first_part(coordinates: str):
-    coordinate_hash = {
-        '>': (1, 0),
-        '<': (-1, 0),
-        '^': (0, -1),
-        'v': (0, 1)
-    }
+def move(current_position, coordinate):
+    correction = COORDINATE_HASH_MAP[coordinate]
+    return (current_position[0] + correction[0], current_position[1] + correction[1])
 
+
+def solution_to_the_first_part(coordinates: str):
     houses = set()
-    current_position = (0,0)
+    current_position = (0, 0)
+
     for coordinate in coordinates:
         houses.add(current_position)
 
-        correction = coordinate_hash[coordinate]
-        current_position = (current_position[0] + correction[0], current_position[1] + correction[1])
+        current_position = move(current_position, coordinate)
+
+    return len(houses)
+
+
+def solution_to_the_second_part(coordinates: str):
+    houses = set()
+
+    current_santa = (0, 0)
+    waiting_santa = (0, 0)
+   
+    for coordinate in coordinates:
+        houses.add(current_santa)
+        
+        current_santa = move(current_santa, coordinate)
+
+        current_santa, waiting_santa = waiting_santa, current_santa
 
     return len(houses)
 
 
 # The solution is taken from: https://adventofcode.com/2015/day/3/input
-print("Solution for the first part:", solution_to_the_first_part(load_input_file('input.03.txt')))
+task_input = load_input_file('input.03.txt')
+print("Solution for the first part:", solution_to_the_first_part(task_input))
+print("Solution for the second part:", solution_to_the_second_part(task_input))
