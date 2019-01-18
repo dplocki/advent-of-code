c2412d687237eca05d125e4d4a67fae576d6e0dd
Dawid Płocki <dawid.plocki@gmail.com>
2019-01-18 20:19:01 +0100

    Solution for 4th task (second part) in 2015

--- 2015/2015_04.py
+++ 2015/2015_04.py
@@ -1,22 +1,62 @@
 import hashlib
+import multiprocessing
 
 
-def load_input_file(file_name):
+def load_input_file(file_name: str):
     with open(file_name) as file:
         return file.read()
 
 
-def findTheLowestIndex(key):
-    check = '0' * 5
-    i = 0
+def generate_hashes(key: str, start_index: int, loop_index):
+    index = start_index
     while True:
-        if hashlib.md5((key + str(i)).encode()).hexdigest().startswith(check):
-            return i
-
-        i += 1
+        yield index, hashlib.md5((key + str(index)).encode()).hexdigest()
+        index += loop_index
 
 
-key = load_input_file('input.04.txt')
+def find_index_starts_with(hash_generator, starts_with: str, result: multiprocessing.Queue):
+    for index, hash in hash_generator:
+        if hash.startswith(starts_with):
+            result.put(index)
+
+
+def worker(key: str, starts_with: str, start_index: int, loop_index: int, result: multiprocessing.Queue):
+    generator = generate_hashes(key, start_index, loop_index)
+    find_index_starts_with(generator, starts_with, result)
+
+
+def find_the_lowest_index(key: str, how_many_zeros_on_begin: int):
+    cpu_count = multiprocessing.cpu_count()
+    answer = multiprocessing.Queue()
+    check = '0' * how_many_zeros_on_begin
+
+    jobs = [
+        multiprocessing.Process(target=worker, args=(
+            key,
+            check,
+            cpu_index,
+            cpu_count,
+            answer)
+        )
+        for cpu_index in range(cpu_count)
+    ]
+
+    for j in jobs:
+        j.start()
+
+    result = answer.get()
+
+    for j in jobs:
+        j.terminate()
+
+    return result
+
 
 # The solution is taken from: https://adventofcode.com/2015/day/4/input
-print("Solution for the first part:", findTheLowestIndex(key))
+key = load_input_file('input.04.txt')
+
+print("Solution for the first part:", find_the_lowest_index(key, 5))
+
+# The solution for it is not deterministic, but I decided to leave it anyway:
+# It is my first Advent task in which I used the concurrent programming
+print("Solution for the second part:", find_the_lowest_index(key, 6))
