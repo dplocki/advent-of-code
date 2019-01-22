325607528294818015d2f5e5e29ce0a05d660c83
Dawid Płocki <dawid.plocki@gmail.com>
2019-01-22 19:34:04 +0100

    Solution for 13th task (second part) in 2015

--- 2015/2015_13.py
+++ 2015/2015_13.py
@@ -7,10 +7,29 @@ def load_input_file(file_name):
             yield line.strip()
 
 
-def solution_for_first_part(lines: [str]):
+def build_grap(lines: [str]) -> dict:
 
 
-    def generate_paths(graph: {}, from_person: str, taken_seats: [], current_happines_level: int):
+    def parse_input(lines: [str]):
+        pattern = re.compile(r'(\w+) would (lose|gain) (\d+) happiness units by sitting next to (\w+).')
+        for line in lines:
+            groups = pattern.match(line)
+            yield groups[1], (-1 if groups[2] == 'lose' else 1) * int(groups[3]), groups[4]
+
+
+    result = {}
+    for who, how_much, to_whom in parse_input(lines):
+        tmp = result.get(who, {})
+        tmp[to_whom] = how_much
+        result[who] = tmp
+
+    return result
+
+
+def find_maximum_happines_level(graph: {}) -> int:
+
+
+    def generate_paths(graph: {}, from_person: str, taken_seats: [], current_happiness_level: int):
         posible_neigborns = set(person for person in graph[from_person].keys() if person not in taken_seats)
 
         if posible_neigborns:
@@ -23,39 +42,32 @@ def solution_for_first_part(lines: [str]):
                         graph,
                         neigborn,
                         taken_seats,
-                        current_happines_level + level_delta)
+                        current_happiness_level + level_delta)
 
                 taken_seats.remove(neigborn)
         else:
-            current_happines_level += graph[from_person][taken_seats[0]]  # circle is closing
-            current_happines_level += graph[taken_seats[0]][from_person]
+            current_happiness_level += graph[from_person][taken_seats[0]]  # circle is closing
+            current_happiness_level += graph[taken_seats[0]][from_person]
             
-            yield current_happines_level
+            yield current_happiness_level
 
 
-    def build_grap(lines: [str]) -> dict:
-
-
-        def parse_input(lines: [str]):
-            pattern = re.compile(r'(\w+) would (lose|gain) (\d+) happiness units by sitting next to (\w+)')
-            for line in lines:
-                groups = pattern.match(line)
-                yield groups[1], (-1 if groups[2] == 'lose' else 1) * int(groups[3]), groups[4]
-
-
-        result = {}
-        for who, how_much, to_whom in parse_input(lines):
-            tmp = result.get(who, {})
-            tmp[to_whom] = how_much
-            result[who] = tmp
-
-        return result
-
-
-    graph = build_grap(lines)
     first = next(graph.keys().__iter__())
-
     return max(happiness_level for happiness_level in generate_paths(graph, first, [first], 0))
+
+
+def solution_for_first_part(graph: {}) -> int:
+    return find_maximum_happines_level(graph)
+
+
+def solution_for_second_part(graph: {}) -> int:
+    people = graph.keys()
+    for person in people:
+        graph[person]['Me'] = 0
+
+    graph['Me'] = {person:0 for person in people}
+    
+    return find_maximum_happines_level(graph)
 
 
 test_input = '''Alice would gain 54 happiness units by sitting next to Bob.
@@ -72,8 +84,11 @@ David would lose 7 happiness units by sitting next to Bob.
 David would gain 41 happiness units by sitting next to Carol.'''
 
 
-assert solution_for_first_part(test_input.splitlines()) == 330
-
+test_graph = build_grap(test_input.splitlines())
+assert solution_for_first_part(test_graph) == 330
 
 # The solution is taken from: https://adventofcode.com/2015/day/13/input
-print("Solution for the first part:", solution_for_first_part(load_input_file('input.13.txt')))
+graph = build_grap(load_input_file('input.13.txt'))
+
+print("Solution for the first part:", solution_for_first_part(graph))
+print("Solution for the second part:", solution_for_second_part(graph))
