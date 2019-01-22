60fe8e4960e2b634229e16d3aee7c27545683ebd
Dawid Płocki <dawid.plocki@gmail.com>
2019-01-22 17:45:40 +0100

    Solution for 12th task (second part) in 2015

--- 2015/2015_12.py
+++ 2015/2015_12.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 
@@ -7,7 +8,23 @@ def load_input_file(file_name):
 
 
 pattern = re.compile(r'-?\d+')
-input = load_input_file('input.12.txt')
 
 # The solution is taken from: https://adventofcode.com/2015/day/12/input
-print("Solution for the first part:", sum(map(int, re.findall(pattern, input))))
+json_data = load_input_file('input.12.txt')
+
+print("Solution for the first part:", sum(map(int, re.findall(pattern, json_data))))
+
+
+def return_value(o) -> int:
+    if type(o) is list:
+        return sum(map(return_value, o))
+    elif type(o) is dict:
+        values = o.values()
+        return 0 if "red" in values else sum(map(return_value, values))
+    elif type(o) is str:
+        return 0
+    else:
+        return o
+
+
+print("Solution for the second part:", return_value(json.loads(json_data)))
