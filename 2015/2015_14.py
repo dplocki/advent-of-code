ab1ce5fc7deea562fd20a4c807a72bf629b37b06
Dawid Płocki <dawid.plocki@gmail.com>
2019-01-23 16:42:43 +0100

    Solution for 14th task (second part) in 2015

--- 2015/2015_14.py
+++ 2015/2015_14.py
@@ -1,6 +1,9 @@
 import re
 
 
+TIME_LIMIT = 2503
+
+
 def load_input_file(file_name):
     with open(file_name) as file:
         for line in file:
@@ -34,12 +37,33 @@ def simulate_race(lines: [str]):
     }
 
     results = {}
-    for i in range(2503):
+    for _ in range(TIME_LIMIT):
         for name, reindeer in reindeers.items():
             results[name] = results.get(name, 0) + next(reindeer)
 
-    return results
+        yield results
+
+
+def solution_for_first_part(lines: [str]):
+    for results in simulate_race(lines):
+        pass
+    
+    return max(results.values())
+
+
+def solution_for_second_part(lines: [str]):
+    points = {}
+
+    for results in simulate_race(lines):
+        maximum = max(results.values())
+
+        for name, value in results.items():
+            if value == maximum:
+                points[name] = points.get(name, 0) + 1
+    
+    return max(points.values())
 
 
 # The solution is taken from: https://adventofcode.com/2015/day/14/input
-print("Solution for the first part:", max(simulate_race(load_input_file('input.14.txt')).values()))
+print("Solution for the first part:", solution_for_first_part(load_input_file('input.14.txt')))
+print("Solution for the second part:", solution_for_second_part(load_input_file('input.14.txt')))
