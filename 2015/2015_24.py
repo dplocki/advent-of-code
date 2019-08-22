202ca4871cba41597bf77145d6697c525b0e69e9
Dawid Płocki <dawid.plocki@gmail.com>
2019-08-22 21:05:55 +0200

    Solution for 24th task (second part) in 2015

--- 2015/2015_24.py
+++ 2015/2015_24.py
@@ -7,25 +7,21 @@ def load_input_file(file_name):
         yield from (int(line.strip()) for line in file)
 
 
-def solution_for_first_part(input: [int]):
+def solution(all_packages_list: [int], how_many_groups):
 
     def subset_sum(numbers, target, partial=[]):
         sum_of_current = sum(partial)
 
-        # check if the partial sum is equals to target
         if sum_of_current == target:
             yield partial
 
         if sum_of_current >= target:
-            return  # if we reach the number why bother to continue
+            return
 
         for index, number in enumerate(numbers):
             yield from subset_sum(numbers[index + 1:], target, partial + [number]) 
 
-
-    all_packages_list = list(input)
-    single_group_size = sum(all_packages_list) // 3
-
+    single_group_size = sum(all_packages_list) // how_many_groups
     all_combination_for_first_group = list(subset_sum(all_packages_list, single_group_size))
 
     min_first_group_length = reduce(lambda x, y: min(x, len(y)), all_combination_for_first_group, sys.maxsize)
@@ -36,4 +32,7 @@ def solution_for_first_part(input: [int]):
     return sorted_first_group_combinations[0][1]
 
 
-print("Solution for the first part:", solution_for_first_part(load_input_file('input.24.txt')))
+# The input is taken from: https://adventofcode.com/2015/day/24/input
+input_list = list(load_input_file('input.24.txt'))
+print("Solution for the first part:", solution(input_list, 3))
+print("Solution for the second part:", solution(input_list, 4))
