aa0b9b934b476bd50dd02bdcdab231e8efe99499
Dawid Płocki <dawid.plocki@gmail.com>
2019-08-24 14:30:46 +0200

    Solution for third task (second part) in 2016

--- 2016/2016_03.py
+++ 2016/2016_03.py
@@ -8,17 +8,40 @@ def parse(lines):
         yield list(map(lambda x: int(x), line.split()))
 
 
-def solution_for_first_part(input_list: [[int]]):
+def find_all_posible_triangles(trinagles_list: [[int]]) -> int:
     result = 0
-
-    for trinagle in input_list:
-        trinagle.sort()
+    for raw_trinagle in trinagles_list:
+        trinagle = sorted(raw_trinagle)
         if trinagle[2] < trinagle[0] + trinagle[1]:
             result += 1
 
     return result
 
 
+def transform_list(input_list: [[int]]) -> [[int]]:
+    triangle_1 = []
+    triangle_2 = []
+    triangle_3 = []
+
+    for t1, t2, t3 in input_list:
+        triangle_1.append(t1)
+        triangle_2.append(t2)
+        triangle_3.append(t3)
+
+        if len(triangle_1) == 3:
+            yield triangle_1
+            yield triangle_2
+            yield triangle_3
+
+            triangle_1 = []
+            triangle_2 = []
+            triangle_3 = []
+        
+
+def solution_for_second_part(input_list: [[int]]) -> int:
+    return find_all_posible_triangles(transform_list(input_list))
+
 # The input is taken from: https://adventofcode.com/2016/day/3/input
-input_list = parse(load_input_file('input.03.txt'))
-print("Solution for the first part:", solution_for_first_part(input_list))
+input_list = list(parse(load_input_file('input.03.txt')))
+print("Solution for the first part:", find_all_posible_triangles(input_list))
+print("Solution for the second part:", solution_for_second_part(input_list))
