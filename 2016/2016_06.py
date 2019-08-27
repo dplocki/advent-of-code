f9f3cd3b10e7d1d736895f3f14a69044da157ecc
Dawid Płocki <dawid.plocki@gmail.com>
2019-08-27 18:59:56 +0200

    Solution for 6th task (first part) in 2016

---
+++ 2016/2016_06.py
@@ -0,0 +1,32 @@
+import operator
+
+
+MESSAGE_LENGTH = 8
+
+
+def load_input_file(file_name):
+    with open(file_name) as file:
+        yield from (line.strip() for line in file)
+
+
+def calculate_the_letter_occurents(input_lines: [str]) -> [dict]:
+    result = [{} for _ in range(MESSAGE_LENGTH)]
+
+    for input_line in input_lines:
+        for index, value in enumerate(input_line):
+            result[index][value] = result[index].get(value, 0) + 1
+
+    return result
+
+
+def get_the_most_common_letter(position_occurent: [dict]):
+    for position_occurent in position_occurent:
+        yield max(position_occurent, key=position_occurent.get)
+
+
+def solution_for_first_part(input: [str]) -> str:
+    return ''.join(get_the_most_common_letter(calculate_the_letter_occurents(input)))
+
+
+# The input is taken from: https://adventofcode.com/2016/day/6/input
+print("Solution for the first part:", solution_for_first_part(load_input_file('input.06.txt')))
