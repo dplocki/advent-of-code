587f6ba772ab34d575d006cb7992bbee945996ed
Dawid Płocki <dawid.plocki@gmail.com>
2019-08-30 21:48:01 +0200

    Solution for 10th task (second part) in 2016

--- 2016/2016_10.py
+++ 2016/2016_10.py
@@ -6,57 +6,67 @@ def load_input_file(file_name):
         yield from (line.strip() for line in file)
 
 
-def parse_line(lines: [str]):
-    value_line = re.compile(r'value (\d+) goes to bot (\d+)')
-    instruction_line = re.compile(r'bot (\d+) gives low to ((bot|output) (\d+)) and high to ((bot|output) (\d+))')
+class Factory:
 
-    initial_values = {}
-    instructions = {}
+    def __init__(self):
+        self.values = None
+        self.instructions = None
 
-    for line in lines:
-        if line.startswith('value'):
-            groups = value_line.match(line)
-            bot_name = f'bot {groups[2]}'
-
-            bot = initial_values.get(bot_name, [])
-            bot.append(int(groups[1]))
-            initial_values[bot_name] = bot
-        else:
-            groups = instruction_line.match(line)
-            instructions[f'bot {groups[1]}'] = (groups[2], groups[5])
-
-    return instructions, initial_values
-
-
-def follow_instruction(instructions: {}, values: {}):
-
-
-    def add_value_to_bot(bot_name, value):
-        tmp = values.get(bot_name, [])
+    def add_value_to_bot(self, bot_name, value):
+        tmp = self.values.get(bot_name, [])
         tmp.append(value)
 
-        values[bot_name] = tmp
+        self.values[bot_name] = tmp
+
+    def parse_line(self, lines: [str]):
+        value_line = re.compile(r'value (\d+) goes to bot (\d+)')
+        instruction_line = re.compile(r'bot (\d+) gives low to ((bot|output) (\d+)) and high to ((bot|output) (\d+))')
+
+        self.values = {}
+        self.instructions = {}
+
+        for line in lines:
+            if line.startswith('value'):
+                groups = value_line.match(line)
+                bot_name = f'bot {groups[2]}'
+
+                self.add_value_to_bot(bot_name, int(groups[1]))
+            else:
+                groups = instruction_line.match(line)
+                self.instructions[f'bot {groups[1]}'] = (groups[2], groups[5])
+        
+    def follow_instruction(self):
+        while True:
+            key = next((key for key, value in self.values.items() if len(value) == 2 and key.startswith('bot ')), None)
+            if key == None:
+                return
+
+            low_value, high_value = sorted(self.values[key])
+
+            yield key, low_value, high_value
+
+            low_value_bot, high_value_bot = self.instructions[key]
+
+            self.values[key] = []
+            self.add_value_to_bot(low_value_bot, low_value)
+            self.add_value_to_bot(high_value_bot, high_value)
 
 
-    while True:
-        key = next(key for key, value in values.items() if len(value) == 2 and key.startswith('bot '))
-        low_value, high_value = sorted(values[key])
-
-        yield key, low_value, high_value
-
-        low_value_bot, high_value_bot = instructions[key]
-
-        values[key] = []
-        add_value_to_bot(low_value_bot, low_value)
-        add_value_to_bot(high_value_bot, high_value)
-
-
-def solution_for_the_first_part(instructions: {}, values: {}):
-    for bot_name, low_value, high_value in follow_instruction(instructions, values):
+def solution_for_the_first_part(factory: Factory) -> str:
+    for bot_name, low_value, high_value in factory.follow_instruction():
         if low_value == 17 and high_value == 61:
             return bot_name
 
 
+def solution_for_the_second_part(factory: Factory) -> int:
+    for _ in factory.follow_instruction():
+        pass
+    
+    return factory.values['output 0'][0] * factory.values['output 1'][0] * factory.values['output 2'][0]
+
+
 # The input is taken from: https://adventofcode.com/2016/day/10/input
-instructions, initial_values = parse_line(load_input_file('input.10.txt'))
-print("Solution for the first part:", solution_for_the_first_part(instructions, initial_values))
+factory = Factory()
+factory.parse_line(load_input_file('input.10.txt'))
+print("Solution for the first part:", solution_for_the_first_part(factory))
+print("Solution for the second part:", solution_for_the_second_part(factory))
