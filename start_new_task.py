9159f3f5b48c877392c18237bc27dd1f32458f84
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-01 08:44:00 +0100

    Allowing creating solutions for previous years in December

--- start_new_task.py
+++ start_new_task.py
@@ -45,21 +45,32 @@ def open_tabs(year: int, day: int):
     webbrowser.open(f'https://adventofcode.com/{year}/day/{day}')
 
 
+def is_solution_for_day_exist(year, day):
+    day_with_zero = str(day).zfill(2)
+    file_name = f'{year}_{day_with_zero}.py'
+    return os.path.exists(os.path.join(str(year), file_name))
+
+
 def find_first_missing(today):
     for year in range(2015, today.year + 1):
-        year_with_zero = str(year)
-        if not os.path.exists(year_with_zero):
+        directory_name = str(year)
+        if not os.path.exists(directory_name):
             return year, 1
 
         for day in range(1, 26):
-            day_with_zero = str(day).zfill(2)
-            file_name = f'{year_with_zero}_{day_with_zero}.py'
-            if not os.path.exists(os.path.join(year_with_zero, file_name)):
+            if not is_solution_for_day_exist(year, day):
                 return year, day
 
 
+def for_which_date_create(input_date):
+    if input_date.month == 12 and not is_solution_for_day_exist(input_date.year, input_date.day):
+        return today.year, today.day
+    else:
+        return find_first_missing(today)
+
+
 today = date.today()
-year, day = (today.year, today.day) if today.month == 12 else find_first_missing(today)
+year, day = for_which_date_create(today)
 
 create_dictionary(year)
 create_files(year, day)
