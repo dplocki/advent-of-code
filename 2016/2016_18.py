a4ba5cdedc0efa4765e22f21290e71a7a990a1cb
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-03 21:18:58 +0100

    Solution for 18th task (second part) in 2016

--- 2016/2016_18.py
+++ 2016/2016_18.py
@@ -9,28 +9,28 @@ def next_row(prev_row):
         length = len(prev_row)
         for i in range(length):
             left = bool(i == 0) or prev_row[i - 1]
-            center = prev_row[i]
             right = bool(i + 1 >= length) or prev_row[i + 1]
 
-            yield not ((not left and not center and right) \
-                or (not center and not right and left) \
-                or (not left and center and right) \
-                or (left and center and not right))
+            yield left == right
 
 
     return list(build_row(prev_row))
 
 
-def solution_for_first_part(first_row):
+def generate_rows(row, how_much):
+    for _ in range(how_much):
+        yield row
+        row = next_row(row)
 
-    def generate_40_rows(row):
-        for _ in range(40):
-            yield row
-            row = next_row(row)
 
-    return sum([sum([1 for c in row if c]) for row in generate_40_rows(first_row)])
+def count_safe_tiles(first_row, how_much):
+    return sum([
+            row.count(True)
+            for row in generate_rows(first_row, how_much)
+        ])
 
 
 # The input is taken from: https://adventofcode.com/2016/day/18/input
 first_row = load_input_file('input.18.txt')
-print("Solution for the first part:", solution_for_first_part(first_row))
+print("Solution for the first part:", count_safe_tiles(first_row, 40))
+print("Solution for the second part:", count_safe_tiles(first_row, 400_000))
