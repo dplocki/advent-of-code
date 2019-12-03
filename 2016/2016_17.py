20e17b1ac2e16c5a19328658728d99cd00ac69c4
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-03 18:41:35 +0100

    Solution for 17th task (second part) in 2016

--- 2016/2016_17.py
+++ 2016/2016_17.py
@@ -1,7 +1,9 @@
 import hashlib
+from collections import deque
 
 
 MAZE_SIZE = 4
+LAST_COORDINATE = MAZE_SIZE - 1
 
 
 def md5(from_string: str) -> str:
@@ -13,7 +15,7 @@ def load_input_file(file_name: str):
         return file.read()
 
 
-def solution_for_first_part(passcode):
+def path_generator(passcode):
 
     def get_posibilities(point, path, passcode):
         x, y = point
@@ -23,34 +25,48 @@ def solution_for_first_part(passcode):
         if y > 0 and doors[0]:
             yield (x, y - 1), path + 'U'
 
-        if y < MAZE_SIZE and doors[1]:
+        if y < LAST_COORDINATE and doors[1]:
             yield (x, y + 1), path + 'D'
 
         if x > 0 and doors[2]:
             yield (x - 1, y), path + 'L'
 
-        if x < MAZE_SIZE and doors[3]:
+        if x < LAST_COORDINATE and doors[3]:
             yield (x + 1, y), path + 'R'
 
-        return 
 
-
-    end = (MAZE_SIZE - 1, MAZE_SIZE -1)
-    posibilities = [((0, 0), '')]
+    end = (LAST_COORDINATE, LAST_COORDINATE)
+    posibilities = deque([((0, 0), '')])
     while posibilities:
-        point, path = posibilities.pop(0)
+        point, path = posibilities.popleft()
 
         if point == end:
-            return path
+            yield path
+            continue
 
         for new_point, new_path in get_posibilities(point, path, passcode):
             posibilities.append((new_point, new_path))
 
 
+def solution_for_first_part(passcode):
+    return next(path_generator(passcode))
+
+
 assert solution_for_first_part('ihgpwlah') == 'DDRRRD'
 assert solution_for_first_part('kglvqrro') == 'DDUDRLRRUDRD'
-#assert solution_for_first_part('ulqzkmiv') == 'DRURDRUDDLLDLUURRDULRLDUUDDDRR' # not working? but the code give me correct solution 
+assert solution_for_first_part('ulqzkmiv') == 'DRURDRUDDLLDLUURRDULRLDUUDDDRR'
 
 # The input is taken from: https://adventofcode.com/2016/day/17/input
 passcode = load_input_file('input.17.txt')
 print("Solution for the first part:", solution_for_first_part(passcode))
+
+
+def solution_for_second_part(passcode):
+    return max([len(i) for i in path_generator(passcode)])
+
+
+assert solution_for_second_part('ihgpwlah') == 370
+assert solution_for_second_part('kglvqrro') == 492
+assert solution_for_second_part('ulqzkmiv') == 830
+
+print("Solution for the second part:", solution_for_second_part(passcode))
