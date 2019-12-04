798b97553cf03620a1256ab71b01b83ddb642462
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-04 07:00:09 +0100

    Solution for 4th task (second part) in 2019

--- 2019/2019_04.py
+++ 2019/2019_04.py
@@ -11,7 +11,7 @@ def pair_previous_with_current(sample):
         previous = current
 
 
-def check(sample):
+def check_for_first_part(sample):
     is_pair = False
 
     for p, c in pair_previous_with_current(sample):
@@ -24,14 +24,33 @@ def check(sample):
     return is_pair
 
 
-def solution_for_first_part(start_range, end_range):
+def count_maching_password_in_range(start_range, end_range, check):
     return sum([1 for sample in range(start_range, end_range + 1) if check(sample)])
 
 
-assert check(112233) == True
-assert check(223450) == False
-assert check(123789) == False
+assert check_for_first_part(112233) == True
+assert check_for_first_part(223450) == False
+assert check_for_first_part(123789) == False
 
 # The input is taken from: https://adventofcode.com/2019/day/4/input
 start_range, end_range = load_input_file('input.04.txt')
-print("Solution for the first part:", solution_for_first_part(start_range, end_range))
+print("Solution for the first part:", count_maching_password_in_range(start_range, end_range, check_for_first_part))
+
+
+def check_for_second_part(sample):
+    groups = '.'
+    for p, c in pair_previous_with_current(sample):
+        if p > c:
+            return False
+
+        groups += '^' if p == c else '.'
+
+    groups += '.'
+    return '.^.' in groups
+
+
+assert check_for_second_part(112233) == True
+assert check_for_second_part(123444) == False
+assert check_for_second_part(111122) == True
+
+print("Solution for the second part:", count_maching_password_in_range(start_range, end_range, check_for_second_part))
