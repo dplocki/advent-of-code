aa0fdfb11a31d00d8b91bd3326402d00bcbde93e
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-05 12:35:03 +0100

    Solution for 20th task (second part) in 2016

--- 2016/2016_20.py
+++ 2016/2016_20.py
@@ -1,28 +1,56 @@
+MINIMUM_IP = 0
+MAXIMUM_IP = 4294967295
+
+
 def load_input_file(file_name):
     with open(file_name) as file:
         for line in file:
             yield tuple(map(int, line.strip().split('-')))
 
 
-def solution_for_first_part(input_list: (int, int)) -> int:
+def find_first_allow_ip(blocked_ips_list: [int], from_ip: int) -> int:
 
-    def get_blocking_rule_for(ip, blocked_ips_list):
-        for ip_range in blocked_ips_list:
+    def get_blocking_rule_for(ip, blocked_ips_sublist):
+        for ip_range in blocked_ips_sublist:
             if ip >= ip_range[0] and ip <= ip_range[1]:
                 return ip_range
 
         return None
 
-    blocked_ips_list = list(input_list)
-    ip = 0
-
-    while True:
-        rule = get_blocking_rule_for(ip, blocked_ips_list)
+    rules = blocked_ips_list
+    ip = from_ip
+    while ip <= MAXIMUM_IP:
+        rule = get_blocking_rule_for(ip, rules)
         if rule == None:
             return ip
 
         ip = rule[1] + 1
+        rules = list(filter(lambda x: ip <= x[1], rules))
+
+    return None
+
+
+def solution_for_first_part(blocked_ips_list: (int, int)) -> int:
+    return find_first_allow_ip(blocked_ips_list[:], MINIMUM_IP)
+
 
 # The input is taken from: https://adventofcode.com/2016/day/20/input
-input_list = load_input_file('input.20.txt')
-print("Solution for the first part:", solution_for_first_part(input_list))
+blocked_ips_list = list(load_input_file('input.20.txt'))
+print("Solution for the first part:", solution_for_first_part(blocked_ips_list))
+
+
+def solution_for_second_part(blocked_ips_list: (int, int)) -> int:
+    result = 0
+    ip = find_first_allow_ip(blocked_ips_list, MINIMUM_IP)
+    rules = blocked_ips_list
+
+    while ip != None:
+        rules = sorted(filter(lambda x: ip < x[1], rules), key=lambda x: x[0])
+        first_matching_rule = rules[0]
+        result += first_matching_rule[0] - ip
+        ip = find_first_allow_ip(rules, first_matching_rule[1] + 1)
+
+    return result
+
+
+print("Solution for the second part:", solution_for_second_part(blocked_ips_list))
