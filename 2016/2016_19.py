f3c080a10d0954dbf5bd1aac1a28cdd9260aaf5c
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-05 11:18:00 +0100

    Solution for 19th task (second part) in 2016

--- 2016/2016_19.py
+++ 2016/2016_19.py
@@ -6,11 +6,24 @@ def load_input_file(file_name):
         return int(file.read().strip())
 
 
-def numer_elves_with_all_presents(elves_number: int) -> int:
+def solution_for_first_part(elves_number: int) -> int:
     last_2_power = math.floor(math.log(elves_number, 2))
 
     return (elves_number - 2 ** last_2_power) * 2 + 1
 
+
 # The input is taken from: https://adventofcode.com/2016/day/19/input
 elves_number = load_input_file('input.19.txt')
-print("Solution for the first part:", numer_elves_with_all_presents(elves_number))
+print("Solution for the first part:", solution_for_first_part(elves_number))
+
+
+def solution_for_second_part(elves_number: int) -> int:
+    power_3_last = 3 ** math.floor(math.log(elves_number - 1, 3))
+
+    if elves_number <= power_3_last * 2:
+        return elves_number - power_3_last
+    else:
+        return 2 * elves_number - 3 * power_3_last
+
+
+print("Solution for the second part:", solution_for_second_part(elves_number))
