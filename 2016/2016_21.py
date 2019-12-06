82dffe7e24025228b31ba740193b654b083cee4f
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-06 15:22:57 +0100

    Solution for 21th task (second part) in 2016

--- 2016/2016_21.py
+++ 2016/2016_21.py
@@ -57,12 +57,15 @@ def parse(instruction_list: [str]):
             yield move, int(tokens[2]), int(tokens[5])
 
 
-def solution_for_first_part(instruction_list: [str], password):
+def run_parser(parser, instruction_list: [str], password):
     raw_password = list(password)
-    for instruction, first_parameter, second_parameter in parse(instruction_list):
+    for instruction, first_parameter, second_parameter in parser(instruction_list):
         raw_password = instruction(raw_password, first_parameter, second_parameter)
 
     return ''.join(raw_password)
+
+def solution_for_first_part(instruction_list, password):
+    return run_parser(parse, instruction_list, password)
 
 
 assert swap_positions(list('abcde'), 4, 0) == list('ebcda')
@@ -81,10 +84,59 @@ rotate left 1 step
 move position 1 to position 4
 move position 3 to position 0
 rotate based on position of letter b
-rotate based on position of letter d'''
+rotate based on position of letter d'''.splitlines()
 
-assert solution_for_first_part(test_input.splitlines(), 'abcde') == 'decab'
+assert solution_for_first_part(test_input, 'abcde') == 'decab'
 
 # The input is taken from: https://adventofcode.com/2016/day/21/input
-instructions = load_input_file('input.21.txt')
+instructions = list(load_input_file('input.21.txt'))
 print("Solution for the first part:", solution_for_first_part(instructions, 'abcdefgh'))
+
+
+def solution_for_second_part(instruction_list: [str], password):
+
+    def anty_rotate_based(value_table: {}):
+        def anty_rotate_based_with_value_table(password, a, _):
+            index_of_a = password.index(a)
+            return rotate_left(password, value_table[index_of_a], _)
+
+        return anty_rotate_based_with_value_table
+
+    def reversing_parser(original_parser):
+
+        def build_anty_rotate_based_to_left_table():
+            base = list('abcdefgh')
+            for letter in base:
+                reversed_password = rotate_based(base[:], letter, None)
+                yield reversed_password.index(letter), reversed_password.index('a')
+
+        anty_rotate_based_to_left_table = {k: v for k, v in build_anty_rotate_based_to_left_table()}
+
+        reversed_instruction_list = reversed(list(parse(instruction_list)))
+        for instruction, first_parameter, second_parameter in reversed_instruction_list:
+            if instruction in [swap_positions, swap_letters, move]:
+                yield instruction, second_parameter, first_parameter
+            elif instruction == rotate_left:
+                yield rotate_right, first_parameter, second_parameter
+            elif instruction == rotate_right:
+                yield rotate_left, first_parameter, second_parameter
+            elif instruction == rotate_based:
+                yield anty_rotate_based(anty_rotate_based_to_left_table), first_parameter, second_parameter
+            else:
+                yield instruction, first_parameter, second_parameter
+
+
+    return run_parser(reversing_parser, instruction_list, password)
+
+
+def test_of_descrambling(instructions, password):
+    reversed_password = solution_for_first_part(instructions, password)
+    result = solution_for_second_part(instructions, reversed_password)
+    assert result == password, f"Expected '{password}' recived: '{result}'"
+
+
+for l in 'abcdefgh':
+    test_of_descrambling([f'rotate based on position of letter {l}'], 'abcdefgh')
+
+
+print("Solution for the second part:", solution_for_second_part(instructions, 'fbgdceah'))
