62ebaf34a617b44444c82d01c2131d551720ca94
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-07 07:36:48 +0100

    Solution for 7th task (second part) in 2019

--- 2019/2019_07.py
+++ 2019/2019_07.py
@@ -1,12 +1,15 @@
 import itertools
 
 
+END_OPTCODE = 99
+
+
 def load_input_file(file_name):
     with open(file_name) as file:
         return [int(x) for x in file.read().split(',')]
 
 
-def run_program(memory, initial_input):
+def run_program(memory, initial_input, index=0):
 
     def from_memory(memory, paramater):
         index, mode = paramater
@@ -28,7 +31,6 @@ def run_program(memory, initial_input):
         else:
             raise Exception(f'Uknown mode: {mode}')
 
-    index = 0
     outside = initial_input
 
     while True:
@@ -39,8 +41,8 @@ def run_program(memory, initial_input):
         second_parameter = (index + 2, (cmd // 1000) % 10)
         third_parameter = (index + 3, (cmd // 10000) % 10)
 
-        if optcode == 99:
-            return outside.pop()
+        if optcode == END_OPTCODE:
+            return outside, index, memory
 
         # Calculation
         elif optcode == 1: # adding
@@ -67,6 +69,7 @@ def run_program(memory, initial_input):
         elif optcode == 4: # from input
             outside.append(from_memory(memory, first_parameter))
             index += 2
+            return outside, index, memory
 
         # Jumps
         elif optcode == 5: # jump-if-true
@@ -86,21 +89,28 @@ def run_program(memory, initial_input):
 
         else:
             raise Exception(f'Unknown opcode: "{cmd}"')
-   
+
+
+def run_program_to_finished(memory, initial_input, index=0): 
+    while memory[index] != END_OPTCODE:
+        output, index, memory = run_program(memory, initial_input, index)
+
+    return output.pop()
+
 
 def solution_for_first_part(task_input: [int]):
 
-    def thruster_generator():
+    def thruster_generator(task_input):
         for sample in itertools.permutations(range(5)):
             memory = [task_input[:] for _ in range(5)]
 
             outside = 0
             for i in range(5):
-                outside = run_program(memory[i], [sample[i], outside])
+                outside = run_program_to_finished(memory[i], [sample[i], outside])
 
             yield outside
 
-    return max(thruster_generator())
+    return max(thruster_generator(task_input))
 
 
 assert solution_for_first_part([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]) == 43210
@@ -111,3 +121,39 @@ assert solution_for_first_part([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,
 # The input is taken from: https://adventofcode.com/2019/day/7/input
 memory_input = load_input_file('input.07.txt')
 print("Solution for the first part:", solution_for_first_part(memory_input))
+
+
+def solution_for_second_part(task_input: [int]):
+
+    def thruster_generator(task_input):
+        for sample in itertools.permutations(range(5, 9 + 1)):
+            memories = [task_input[:] for i in range(5)]
+            indexes = [0 for i in range(5)]
+            outside = [0]
+
+            for i in range(5):
+                outside, indexes[i], memories[i] = run_program(memories[i], [sample[i], outside[0]], indexes[i])
+
+            while True:
+                for i in range(5):
+                    outside, indexes[i], memories[i] = run_program(memories[i], outside, indexes[i])
+
+                if memories[4][indexes[4]] == END_OPTCODE:
+                    yield outside[0]
+                    break
+
+
+    return max(thruster_generator(task_input))
+
+
+assert solution_for_second_part([
+        3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+        27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5
+    ]) == 139629729
+assert solution_for_second_part([
+        3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+        -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+        53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10
+    ]) == 18216
+
+print("Solution for the second part:", solution_for_second_part(memory_input))
