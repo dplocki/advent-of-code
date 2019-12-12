8a0f2e2282975db61d9ba4f30f6f000ca06a2478
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-12 18:53:42 +0100

    Anding input reading for first task in 2017

--- 2017/2017_01.py
+++ 2017/2017_01.py
@@ -1,9 +1,14 @@
+def load_input_file(file_name):
+    with open(file_name) as file:
+        return file.read().strip()
+
+
 def solve_captcha(input, step):
     result = 0
-    lenght = len(input)
+    length = len(input)
 
     for index, digit in enumerate(input):
-        prev = input[(index + step) % lenght]
+        prev = input[(index + step) % length]
 
         if digit == prev:
             result += int(digit)
@@ -11,12 +16,8 @@ def solve_captcha(input, step):
     return result
 
 
-def solve_captcha_a(input):
+def solution_for_first_path(input):
     return solve_captcha(input, 1)
-
-
-def solve_captcha_b(input):
-    return solve_captcha(input, int(len(input)/2))
 
 
 def test_sample(input, expected, function):
@@ -25,27 +26,31 @@ def test_sample(input, expected, function):
 
 
 def test_sample_a(input, expected):
-    test_sample(input, expected, solve_captcha_a)
+    test_sample(input, expected, solution_for_first_path)
 
 
-def test_sample_b(input, expected):
-    test_sample(input, expected, solve_captcha_b)
-
-
-# Tests A
 test_sample_a('1234', 0)
 test_sample_a('1122', 3)
 test_sample_a('1111', 4)
 test_sample_a('91212129', 9)
 
-# Tests B
+# The input is taken from: https://adventofcode.com/2017/day/1/input
+captcha = load_input_file('input.01.txt')
+print("Solution for the first part:", solution_for_first_path(captcha))
+
+
+def solution_for_second_path(input):
+    return solve_captcha(input, int(len(input)/2))
+
+
+def test_sample_b(input, expected):
+    test_sample(input, expected, solution_for_second_path)
+
+
 test_sample_b('1212', 6)
 test_sample_b('1221', 0)
 test_sample_b('123425', 4)
 test_sample_b('123123', 12)
 test_sample_b('12131415', 4)
 
-# Resolution
-# Download the https://adventofcode.com/2017/day/1/input
-# print(solve_captch_a(<put here>))
-# print(solve_captch_b(<put here>))
+print("Solution for the second part:", solution_for_second_path(captcha))
