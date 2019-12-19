7dee7360b40aa4621b31c9b10e08777bb17af762
Dawid Płocki <dawid.plocki@gmail.com>
2019-12-19 12:04:17 +0100

    Addind new function in initial template for tasks

--- start_new_task.py
+++ start_new_task.py
@@ -20,20 +20,45 @@ def load_input_file(file_name):
     with open(file_name) as file:
         return file.read().strip()
 
+
 def load_input_file(file_name):
     with open(file_name) as file:
         yield from (line.strip() for line in file)
 
 
+def parse(task_input: [str]):
+    pass
+
+
+def solution_for_first_part(task_input):
+    pass
+
+
 
 print(list(
-    load_input_file('{file_input_name}')
+    parse(load_input_file('{file_input_name}'))
 ))
 
 
-# The input is taken from: https://adventofcode.com/{year}/day/{day}/input
 
+
+# The input is taken from: https://adventofcode.com/{year}/day/{day}/input
+task_input = load_input_file('{file_input_name}')
 # print("Solution for the first part:", )
+
+def visualization(point_dictionary):
+    xs = [x for x, _ in point_dictionary.keys()]
+    ys = [y for _, y in point_dictionary.keys()]
+    max_x = max(xs)
+    max_y = max(ys)
+    min_x = min(xs)
+    min_y = min(ys)
+
+    for y in range(min_y, max_y + 1):
+        for x in range(min_x, max_x + 1):
+            print(point_dictionary[x, y], end='')
+
+        print()
 ''')
 
     with open(file_input_name, 'w') as file:
