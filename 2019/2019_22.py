b4b09e51770c99c976d2fb238001fe7f40bd9f2d
Dawid Płocki <dawid.plocki@gmail.com>
2020-01-05 13:01:05 +0100

    Solution for 22th task (second part) in 2019

--- 2019/2019_22.py
+++ 2019/2019_22.py
@@ -49,7 +49,7 @@ def test(instructions_list, result):
 
 def solution_for_first_part(task_input):
     deck = list(range(10007))
-    return shuffle_cards(deck, parse(task_input)).index(2019)
+    return shuffle_cards(deck, task_input).index(2019)
 
 
 test('''deal with increment 7
@@ -76,5 +76,34 @@ deal with increment 3
 cut -1''', '9 2 5 8 1 4 7 0 3 6')
 
 # The input is taken from: https://adventofcode.com/2019/day/22/input
-task_input = list(load_input_file('input.22.txt'))
+task_input = list(parse(load_input_file('input.22.txt')))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def calculate_increment_and_offset(deck_size: int, instructions: list) -> (int, int):
+    increment, offset = 1, 0
+
+    for instruction, paramater in instructions:
+        if instruction == INSTRUCTION_NEW_STACK:
+            increment = (-1 * increment) % deck_size
+            offset = (offset + increment) % deck_size
+        elif instruction == INSTRUCTION_CUT:
+            offset = (offset + increment * paramater) % deck_size
+        elif instruction == INSTRUCTION_DEAL:
+            increment = (increment * pow(paramater, deck_size - 2, deck_size)) % deck_size
+
+    return increment, offset
+
+
+def solution_for_second_part(instructions: list) -> int:
+    deck_size = 119315717514047
+    shuffle_number = 101741582076661
+    increment_mul, offset_diff = calculate_increment_and_offset(deck_size, instructions)
+    
+    increment = pow(increment_mul, shuffle_number, deck_size)
+    offset = offset_diff * (1 - pow(increment_mul, shuffle_number, deck_size)) * pow(1 - increment_mul, deck_size - 2, deck_size)
+
+    return (increment * 2020 + offset) % deck_size
+
+
+print("Solution for the second part:", solution_for_second_part(task_input))
