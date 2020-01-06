bf397f8424363e39250e614b11aa96fba8af1b3d
Dawid Płocki <dawid.plocki@gmail.com>
2020-01-06 20:54:34 +0100

    Solution for 24th task (second part) in 2019

--- 2019/2019_24.py
+++ 2019/2019_24.py
@@ -1,4 +1,8 @@
+from collections import defaultdict
+
+
 GRID_SIZE = 5
+GRID_CENTER = 2
 
 
 def load_input_file(file_name: str):
@@ -26,7 +30,7 @@ def bugs_generator(grid: dict):
 
 
     def count_neighbor_bugs(grid: dict, x: int, y: int) -> int:
-        return sum([1 if grid.get(neighbor, False) else 0 for neighbor in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]])
+        return sum(1 if grid.get(neighbor, False) else 0 for neighbor in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)])
 
 
     def bug_will_died(grid: dict, x: int, y: int) -> bool:
@@ -38,11 +42,11 @@ def bugs_generator(grid: dict):
 
 
     def calculate_biodiversity_rating(grid: dict) -> int:
-        return sum([i * power for i, power in zip([
+        return sum(i * power for i, power in zip([
             1 if grid[x, y] else 0
             for y in range(GRID_SIZE)
             for x in range(GRID_SIZE)
-        ], power_two_generator())])
+        ], power_two_generator()))
 
     
     def build_new_grid(grid: dict) -> dict:
@@ -59,26 +63,104 @@ def bugs_generator(grid: dict):
         grid = new_grid
 
 
-def solution_for_first_part(task_input: str):
-    grids = parse(task_input)
+def solution_for_first_part(grid: dict):
     been = set()
 
-    for state in bugs_generator(grids):
+    for state in bugs_generator(grid):
         if state in been:
             return state
 
         been.add(state)
 
 
-assert solution_for_first_part('''....#
+assert solution_for_first_part(parse('''....#
 #..#.
 #..##
 ..#..
-#....'''.splitlines()) == 2129920
+#....'''.splitlines())) == 2129920
 
 
 # The input is taken from: https://adventofcode.com/2019/day/24/input
-task_input = load_input_file('input.24.txt')
-print("Solution for the first part:", solution_for_first_part(task_input))
+grid = parse(load_input_file('input.24.txt'))
+print("Solution for the first part:", solution_for_first_part(grid))
 
 
+def bugs_multidemention_generator(grid: dict):
+
+    def bugs_on_single_level(grids: dict, level: int) -> dict:
+        result = {}
+
+        for y in range(GRID_SIZE):
+            for x in range(GRID_SIZE):
+                if x == GRID_CENTER and y == GRID_CENTER:
+                    continue
+                
+                base = sum(
+                        1 if grids[level].get(neighbor, False) else 0
+                        for neighbor in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+                    )
+
+                if x == 0:
+                    base += grids[level - 1].get((1, GRID_CENTER), False)
+                
+                if x == GRID_SIZE - 1:
+                    base += grids[level - 1].get((3, GRID_CENTER), False)
+
+                if y == 0:
+                    base += grids[level - 1].get((GRID_CENTER, 1), False)
+
+                if y == GRID_SIZE - 1:
+                    base += grids[level - 1].get((GRID_CENTER, 3), False)
+
+                if x == 1 and y == 2:
+                    base += sum(1 for i in range(GRID_SIZE) if grids[level + 1].get((0, i), False))
+
+                if x == 2 and y == 1:
+                    base += sum(1 for i in range(GRID_SIZE) if grids[level + 1].get((i, 0), False))
+
+                if x == 3 and y == 2:
+                    base += sum(1 for i in range(GRID_SIZE) if grids[level + 1].get((4, i), False))
+
+                if x == 2 and y == 3:
+                    base += sum(1 for i in range(GRID_SIZE) if grids[level + 1].get((i, 4), False))
+
+                result[x, y] = base == 1 if grids[level].get((x, y), False) else base == 2 or base == 1
+
+        return result
+
+
+    grids = defaultdict(dict)
+    grids[0] = grid
+
+    while True:
+        levels = sorted(grids.keys())
+
+        new_grids = defaultdict(dict)
+        for level in levels + [min(levels) - 1, max(levels) + 1]:
+            temp = bugs_on_single_level(grids, level)
+            if any(temp.keys()):
+                new_grids[level] = temp
+
+        grids = new_grids
+        yield grids
+
+
+def bugs_after_time(grid, minutes):
+    for _, state in zip(range(minutes), bugs_multidemention_generator(grid)):
+        pass
+
+    return sum(list(grid.values()).count(True) for level, grid in state.items())
+
+
+def solution_for_second_part(grid: dict) -> int:
+    return bugs_after_time(grid, 200)
+
+
+assert bugs_after_time(parse('''....#
+#..#.
+#.?##
+..#..
+#....'''.splitlines()), 10) == 99
+
+
+print("Solution for the second part:", solution_for_second_part(grid))
