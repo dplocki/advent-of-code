7dcc2b6ed2f0bde1e855f4b1d3bc3c4d2267315b
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-02 06:44:45 +0100

    Solution for the second task (second part) in 2020

--- 2020/2020_02.py
+++ 2020/2020_02.py
@@ -14,6 +14,12 @@ def parse(lines: [str]):
         yield int(groups[1]), int(groups[2]), groups[3], groups[4]
 
 
+def count_matching_policies(task_input, check_policy):
+    policies = parse(task_input)
+
+    return sum(1 for _ in filter(check_policy, policies))
+
+
 def solution_for_first_part(task_input):
 
     def check_policy(policy):
@@ -22,9 +28,7 @@ def solution_for_first_part(task_input):
 
         return minium <= letter_count <= maximum
 
-
-    policies = parse(task_input)
-    return sum(1 for _ in filter(check_policy, policies))
+    return count_matching_policies(task_input, check_policy)
 
 
 assert solution_for_first_part('''1-3 a: abcde
@@ -34,3 +38,23 @@ assert solution_for_first_part('''1-3 a: abcde
 # The input is taken from: https://adventofcode.com/2020/day/2/input
 task_input = list(load_input_file('input.02.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input):
+
+    def check_policy(policy):
+        first_position, second_position, letter, password = policy
+        password_length = len(password)
+        first_character = password[first_position - 1] if first_position <= password_length else None
+        second_character = password[second_position - 1] if second_position <= password_length else None
+
+        return bool(first_character == letter) ^ bool(second_character == letter)
+
+    return count_matching_policies(task_input, check_policy)
+
+
+assert solution_for_second_part('''1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc'''.splitlines()) == 1
+
+print("Solution for the second part:", solution_for_second_part(task_input))
