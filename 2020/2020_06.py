97c68679c93c9e35e69491d6127996b2e81e8190
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-06 06:57:16 +0100

    Solution for the 6th task (second part) in 2020

--- 2020/2020_06.py
+++ 2020/2020_06.py
@@ -1,22 +1,26 @@
+from functools import reduce
+
+
 def load_input_file(file_name: str) -> [str]:
     with open(file_name) as file:
         yield from (line.strip() for line in file)
 
 
-def parse(lines: [str]) -> [set]:
-    result = set()
-
-    for line in lines:
-        if line == '':
-            yield result
-            result = set()
-
-        result.update((c for c in line))
-
-    yield result
-
-
 def solution_for_first_part(task_input: [str]) -> int:
+
+    def parse(lines: [str]) -> [set]:
+        result = set()
+
+        for line in lines:
+            if line == '':
+                yield result
+                result = set()
+            else:
+                result.update((c for c in line))
+
+        yield result
+
+
     return sum(len(r) for r in parse(task_input))
         
 
@@ -39,3 +43,47 @@ b'''.splitlines()) == 11
 # The input is taken from: https://adventofcode.com/2020/day/6/input
 task_input = list(load_input_file('input.06.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: [str]) -> int:
+
+    def parse(lines: [str]) -> [[set]]:
+        result = []
+
+        for line in lines:
+            if line == '':
+                yield result
+                result = []
+            else:
+                result.append(set(line))
+
+        yield result
+
+    def common_part(a: set, b: set) -> set:
+        return a & b
+
+
+    return sum(
+            len(reduce(common_part, group_responses))
+            for group_responses in parse(task_input)
+        )
+
+
+assert solution_for_second_part('''abc
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b'''.splitlines()) == 6
+
+print("Solution for the second part:", solution_for_second_part(task_input))
