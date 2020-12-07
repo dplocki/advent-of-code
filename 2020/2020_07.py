1a3c9a1d6f02ac90770e962474cbb3c0a7c1a1b3
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-07 17:59:22 +0100

    Optimization of 7th task in 2020

--- 2020/2020_07.py
+++ 2020/2020_07.py
@@ -1,3 +1,9 @@
+from typing import Callable
+
+
+YOUR_BAG = 'shiny gold'
+
+
 def load_input_file(file_name: str):
     with open(file_name) as file:
         return file.read().strip()
@@ -8,7 +14,7 @@ def load_input_file(file_name: str):
         yield from (line.strip() for line in file)
 
 
-def parse(lines: [str]):
+def parse(lines: [str]) -> [str]:
 
     def parse_inside_bag(rule: str) -> (int, [str]):
         how_many, what = rule.split(' ', 1)
@@ -21,18 +27,31 @@ def parse(lines: [str]):
         yield main_bag, [parse_inside_bag(bag) for bag in inside_bags if bag != 'no other bags.']
 
 
+def memoize(func: Callable[[str], int]) -> Callable[[str], int]:
+    memory = {}
+
+    def inner(bag_name: str) -> int:
+        if bag_name not in memory:
+            memory[bag_name] = func(bag_name)
+
+        return memory[bag_name]
+
+    return inner
+
+
 def solution_for_first_part(bags_rule_set: dict) -> int:
 
-    def can_contain_shiny_gold(bags_rule_set: dict, bag: str) -> bool:
+    @memoize
+    def can_contain_shiny_gold(bag: str) -> bool:
         rules = bags_rule_set[bag]
 
-        if 'shiny gold' in (bag_inside[1] for bag_inside in rules):
+        if YOUR_BAG in (bag_inside[1] for bag_inside in rules):
             return True
-        
-        return any(can_contain_shiny_gold(bags_rule_set, bag_inside[1]) for bag_inside in rules)
+
+        return any(can_contain_shiny_gold(bag_inside[1]) for bag_inside in rules)
 
 
-    return sum(1 for bag_name in bags_rule_set.keys() if can_contain_shiny_gold(bags_rule_set, bag_name))
+    return sum(1 for bag_name in bags_rule_set.keys() if can_contain_shiny_gold(bag_name))
 
 
 # The input is taken from: https://adventofcode.com/2020/day/7/input
@@ -42,12 +61,13 @@ print("Solution for the first part:", solution_for_first_part(bags_rule_set))
 
 def solution_for_second_part(bags_rule_set: dict) -> int:
 
+    @memoize
     def count(bag_name: str) -> int:
         inside_bags = bags_rule_set[bag_name]
         return sum(bag[0] + bag[0] * count(bag[1]) for bag in inside_bags)
 
 
-    return count('shiny gold')
+    return count(YOUR_BAG)
 
 
 print("Solution for the second part:", solution_for_second_part(bags_rule_set))
