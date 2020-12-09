2bc054ddcc1b83a2ab68dabb9a9b2430f7ceecea
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-09 06:58:51 +0100

    Solution for the 9th task (second part) in 2020

--- 2020/2020_09.py
+++ 2020/2020_09.py
@@ -2,7 +2,7 @@ from collections import deque
 from itertools import combinations
 
 
-def load_input_file(file_name):
+def load_input_file(file_name: str) -> [int]:
     with open(file_name) as file:
         yield from (int(line.strip()) for line in file)
 
@@ -25,8 +25,28 @@ def solution_for_first_part(task_input: [int], preamble_size: int) -> int:
         last_numbers.append(number)
 
 
-assert solution_for_first_part([35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576], 5) == 127
+assert_example = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
+assert solution_for_first_part(assert_example, 5) == 127
 
 # The input is taken from: https://adventofcode.com/2020/day/9/input
 task_input = list(load_input_file('input.09.txt'))
-print("Solution for the first part:", solution_for_first_part(task_input, 25))
+first_non_maching_number = solution_for_first_part(task_input, 25)
+print("Solution for the first part:", first_non_maching_number)
+
+
+def solution_for_second_part(task_input: [int], request_number: int) -> int:
+    numbers = list(reversed(task_input))
+    contiguous_sequence = deque()
+
+    while True:
+        current_sum = sum(contiguous_sequence)
+        if current_sum == request_number:
+            return min(contiguous_sequence) + max(contiguous_sequence)
+        elif current_sum < request_number:
+            contiguous_sequence.append(numbers.pop())
+        else:
+            contiguous_sequence.popleft()
+
+
+assert solution_for_second_part(assert_example, 127) == 62
+print("Solution for the first part:", solution_for_second_part(task_input, first_non_maching_number))
