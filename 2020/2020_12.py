de6701c12585d0ad4c52e41d4d280154ab854c55
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-12 15:41:35 +0100

    Solution for the 12th task (second part) in 2020

--- 2020/2020_12.py
+++ 2020/2020_12.py
@@ -1,21 +1,10 @@
-from enum import IntEnum, unique
-
-
-@unique
-class Direction(IntEnum):
-    EAST = 0
-    SOUTH = 1
-    WEST = 2
-    NORTH = 3
-
-
 DIRECTION_ROTATE_RIGHT = 'ESWN'
 DIRECTION_NUMBER = len(DIRECTION_ROTATE_RIGHT)
 DIRECTION_VECTOR = {
-    Direction.EAST: (1, 0),
-    Direction.SOUTH: (0, 1),
-    Direction.WEST: (-1, 0),
-    Direction.NORTH: (0, -1)
+    'E': (1, 0),
+    'S': (0, -1),
+    'W': (-1, 0),
+    'N': (0, 1)
 }
 
 
@@ -29,31 +18,31 @@ def parse(task_input: [str]) -> [tuple]:
         yield line[0], int(line[1:])
 
 
+def move(direction: int, value: int, coordinates: tuple) -> tuple:
+    d = DIRECTION_VECTOR[direction]
+    return coordinates[0] + d[0] * value, coordinates[1] + d[1] * value
+
+
 def solution_for_first_part(task_input: [tuple]) -> int:
 
-    def move(direction: int, x: int, y: int) -> int:
-        dx, dy = DIRECTION_VECTOR[direction]
-        return x + dx * value, y + dy * value
+    def turn_right(facing: str, value: int) -> str:
+        return DIRECTION_ROTATE_RIGHT[(DIRECTION_ROTATE_RIGHT.index(facing) + (value // 90)) % DIRECTION_NUMBER]
 
 
-    def turn_right(facing: Direction, value: int) -> Direction:
-        return Direction((facing + (value // 90)) % DIRECTION_NUMBER)
+    ship = 0, 0
+    facing = 'E'
 
-
-    x, y = 0, 0
-    facing = Direction.EAST
-
-    for instruction, value in list(parse(task_input)):
-        if instruction in DIRECTION_ROTATE_RIGHT:
-            x, y = move(DIRECTION_ROTATE_RIGHT.index(instruction), x, y)
+    for instruction, value in parse(task_input):
+        if instruction in DIRECTION_VECTOR:
+            ship = move(instruction, value, ship)
         elif instruction == 'R':
             facing = turn_right(facing, value)
         elif instruction == 'L':
             facing = turn_right(facing, -value)
         elif instruction == 'F':
-            x, y = move(facing, x, y)
+            ship = move(facing, value, ship)
 
-    return abs(x) + abs(y)
+    return sum(map(abs, ship))
 
 
 example_input = '''F10
@@ -66,5 +55,39 @@ assert solution_for_first_part(example_input) == 25
 
 # The input is taken from: https://adventofcode.com/2020/day/12/input
 task_input = list(load_input_file('input.12.txt'))
-result = solution_for_first_part(task_input)
-print("Solution for the first part:", result)
+print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input):
+
+    def turn_right(value, waypoint):
+        x, y = waypoint
+        if value % 360 == 0:
+            return x, y
+        elif value % 360 == 90:
+            return y, -x
+        elif value % 360 == 180:
+            return -x, -y
+        elif value % 360 == 270:
+            return -y, x
+
+
+    ship = 0, 0
+    waypoint = 10, 1
+
+    for instruction, value in parse(task_input):
+        if instruction in DIRECTION_VECTOR:
+            waypoint = move(instruction, value, waypoint)
+        elif instruction == 'R':
+            waypoint = turn_right(value, waypoint)
+        elif instruction == 'L':
+            waypoint = turn_right(abs(360 - value), waypoint)
+        elif instruction == 'F':
+            ship = ship[0] + waypoint[0] * value, ship[1] + waypoint[1] * value
+
+    return sum(map(abs, ship))
+
+
+
+assert solution_for_second_part(example_input) == 286
+print("Solution for the first part:", solution_for_second_part(task_input))
