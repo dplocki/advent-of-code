cfb3f81c1e60217c435fce07d98f042bebeaec72
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-13 12:55:14 +0100

    Solution for the 13th task (second part) in 2020

--- 2020/2020_13.py
+++ 2020/2020_13.py
@@ -1,21 +1,23 @@
+from functools import reduce 
+from operator import mul, itemgetter
+
+
 def load_input_file(file_name: str) -> str:
     with open(file_name) as file:
         return file.read().strip()
 
 
-def parse(task_input: [str]):
-    timestamp, buses = task_input.split('\n')
-    return int(timestamp), (int(x) for x in buses.split(',') if x != 'x')
+def solution_for_first_part(task_input: [str]) -> int:
+
+    def parse(task_input: [str]):
+        timestamp, buses = task_input.splitlines()
+        return int(timestamp), (int(x) for x in buses.split(',') if x != 'x')
 
 
-def solution_for_first_part(task_input):
     timestamp, buses = parse(task_input)
-    earliest_bus = min(
-        ((bus, ((timestamp // bus + 1) * bus) - timestamp) for bus in buses),
-        key=lambda bus_with_arrive_time: bus_with_arrive_time[1]
-    )
+    earliest_bus = min(((bus, ((timestamp // bus + 1) * bus) - timestamp) for bus in buses), key=itemgetter(1))
 
-    return earliest_bus[0] * earliest_bus[1]
+    return reduce(mul, earliest_bus)
 
 
 example_input = '''939
@@ -28,3 +30,53 @@ task_input = load_input_file('input.13.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
 
 
+def solution_for_second_part(task_input: [str]) -> int:
+
+    def parse(task_input: [str]) -> tuple:
+        return (
+            int(x) if x != 'x' else None
+            for x in task_input.splitlines()[1].split(','))
+ 
+    def modular_inverse(a: int, b: int) -> int:
+        if b == 1:
+            return 1
+
+        b0 = b
+        x0, x1 = 0, 1
+        while a > 1:
+            q = a // b
+            a, b = b, a % b
+            x0, x1 = x1 - q * x0, x0
+
+        if x1 < 0:
+            x1 += b0
+
+        return x1
+ 
+
+    def solve_chinese_remainder_theorem(congruences: list) -> int:
+        product = reduce(mul, map(itemgetter(1), congruences))
+
+        solution = 0
+        for a_i, n_i in congruences:
+            p = product // n_i
+            solution += a_i * modular_inverse(p, n_i) * p
+
+        return solution % product
+
+
+    return solve_chinese_remainder_theorem([
+            (-i, bus)
+            for i, bus in enumerate(parse(task_input))
+            if bus != None
+        ])
+
+
+assert solution_for_second_part(example_input) == 1068781
+assert solution_for_second_part('\n17,x,13,19') == 3417
+assert solution_for_second_part('\n67,7,59,61') == 754018
+assert solution_for_second_part('\n67,x,7,59,61') == 779210
+assert solution_for_second_part('\n67,7,x,59,61') == 1261476
+assert solution_for_second_part('\n1789,37,47,1889') == 1202161486
+
+print("Solution for the first part:", solution_for_second_part(task_input))
