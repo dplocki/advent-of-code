34750fa7efa31e52aac63b0ce98a7dd7fcc3b266
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-15 07:26:53 +0100

    Solution for the 15th task (second part) in 2020

--- 2020/2020_15.py
+++ 2020/2020_15.py
@@ -8,12 +8,12 @@ def load_input_file(file_name: str) -> str:
         return parse(file.read().strip())
 
 
-def solution_for_first_part(task_input: [int]):
+def simulator(max_turn, task_input):
     pre_history = {}
     history = {number:i for i, number in enumerate(task_input)}
     last_number_spoken = task_input[-1]
 
-    for turn in range(len(task_input), 2020):
+    for turn in range(len(task_input), max_turn):
         last_number_spoken = history[last_number_spoken] - pre_history.get(last_number_spoken, turn)
         if last_number_spoken <= 0:
             last_number_spoken = 0
@@ -22,6 +22,10 @@ def solution_for_first_part(task_input: [int]):
         history[last_number_spoken] = turn
 
     return last_number_spoken
+
+
+def solution_for_first_part(task_input: [int]):
+    return simulator(2020, task_input)
 
 
 assert solution_for_first_part([0, 3, 6]) == 436
@@ -35,3 +39,18 @@ assert solution_for_first_part([3, 1, 2]) == 1836
 # The input is taken from: https://adventofcode.com/2020/day/15/input
 task_input = list(load_input_file('input.15.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: [int]):
+    return simulator(30000000, task_input)
+
+
+assert solution_for_second_part([0, 3, 6]) == 175594
+assert solution_for_second_part([1, 3, 2]) == 2578
+assert solution_for_second_part([2, 1, 3]) == 3544142
+assert solution_for_second_part([1, 2, 3]) == 261214
+assert solution_for_second_part([2, 3, 1]) == 6895259
+assert solution_for_second_part([3, 2, 1]) == 18
+assert solution_for_second_part([3, 1, 2]) == 362
+
+print("Solution for the second part:", solution_for_second_part(task_input))
