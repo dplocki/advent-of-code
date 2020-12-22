88dfbe00fcca6800f6dbaef97b13258ee7282459
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-22 08:03:27 +0100

    Solution for the 22th task (second part) in 2020

--- 2020/2020_22.py
+++ 2020/2020_22.py
@@ -11,7 +11,7 @@ def load_input_file(file_name: str) -> str:
         return file.read().strip()
 
 
-def parse(task_input: [str]):
+def parse(task_input: [str]) -> tuple:
 
     def parse_deck(raw_deck: str) -> [int]:
         return map(int, raw_deck.splitlines()[1:])
@@ -21,7 +21,11 @@ def parse(task_input: [str]):
     return parse_deck(raw_decks[PLAYER_1_DECK]), parse_deck(raw_decks[PLAYER_2_DECK])
 
 
-def solution_for_first_part(task_input):
+def count_score(winner: [int]) -> int:
+    return sum(value * score for value, score in zip(winner, range(len(winner), 0, -1)))
+
+
+def solution_for_first_part(task_input: str) -> int:
     player1_deck, player2_deck = parse(task_input)
     player1_deck = deque(player1_deck)
     player2_deck = deque(player2_deck)
@@ -37,8 +41,7 @@ def solution_for_first_part(task_input):
             player2_deck.append(p2)
             player2_deck.append(p1)
 
-    winer = player1_deck if player1_deck else player2_deck
-    return sum(value * score for value, score in zip(winer, range(len(winer), 0, -1)))
+    return count_score(player1_deck if player1_deck else player2_deck)
 
 
 example_input = '''Player 1:
@@ -60,3 +63,42 @@ assert solution_for_first_part(example_input) == 306
 # The input is taken from: https://adventofcode.com/2020/day/22/input
 task_input = load_input_file('input.22.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: str) -> int:
+
+    def game(player1_deck: deque, player2_deck: deque) -> tuple:
+        memory = set()
+        
+        while len(player1_deck) > 0 and len(player2_deck) > 0:
+            hash_state = tuple(player1_deck), tuple(player2_deck)
+            if hash_state in memory:
+                return player1_deck, []
+            else:
+                memory.add(hash_state)
+
+            p1 = player1_deck.popleft()
+            p2 = player2_deck.popleft()
+
+            player1_won = \
+                len(game(deque(list(player1_deck)[0:p1]), deque(list(player2_deck)[0:p2]))[PLAYER_1_DECK]) > 0 \
+                if p1 <= len(player1_deck) and p2 <= len(player2_deck) \
+                else p1 > p2
+
+            if player1_won:
+                player1_deck.append(p1)
+                player1_deck.append(p2)
+            else:
+                player2_deck.append(p2)
+                player2_deck.append(p1)
+
+        return player1_deck, player2_deck
+
+
+    player1_deck, player2_deck = parse(task_input)
+    player1_deck, player2_deck = game(deque(player1_deck), deque(player2_deck))
+
+    return count_score(player1_deck if player1_deck else player2_deck)
+
+
+print("Solution for the second part:", solution_for_second_part(task_input))
