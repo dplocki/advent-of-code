9d222b06a66178513c1f4829db5fbd842bea65c7
Dawid Płocki <dawid.plocki@gmail.com>
2020-12-24 08:30:32 +0100

    Solution for the 24th task (second part) in 2020

--- 2020/2020_24.py
+++ 2020/2020_24.py
@@ -1,13 +1,14 @@
+from itertools import chain
+
+
 def load_input_file(file_name: str) -> [str]:
     with open(file_name) as file:
         yield from (line.strip() for line in file)
 
 
-def solution_for_first_part(task_input):
-    lines = list(task_input)
-
-    all_tiles = {}
-    for line in lines:
+def get_black_tiles(instructions: [str]) -> set:
+    result = set()
+    for line in instructions:
         x, y, z = 0, 0, 0
 
         steps = list(line[::-1])
@@ -29,9 +30,16 @@ def solution_for_first_part(task_input):
                 elif step == 'ne':
                     x, y, z = x + 1, y, z - 1
 
-        all_tiles[(x, y, z)] = not all_tiles.get((x, y, z), True)
+        if (x, y, z) in result:
+            result.discard((x, y, z))
+        else:
+            result.add((x, y, z))
 
-    return sum(1 for i in all_tiles.values() if not i)
+    return result
+
+
+def solution_for_first_part(task_input: [str]) -> int:
+    return len(get_black_tiles(task_input))
 
 
 example_input = '''sesenwnenenewseeswwswswwnenewsewsw
@@ -60,3 +68,49 @@ assert solution_for_first_part(example_input) == 10
 # The input is taken from: https://adventofcode.com/2020/day/24/input
 task_input = list(load_input_file('input.24.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def get_neighorns(x: int, y: int, z: int) -> [tuple]:
+    yield x, y + 1, z - 1
+    yield x + 1, y, z - 1
+    yield x + 1, y - 1, z
+    yield x, y - 1, z + 1
+    yield x - 1, y, z + 1
+    yield x - 1, y + 1, z
+
+
+def count_black_neighorns(black_tiles: set, coordinates: tuple) -> int:
+    return sum(1 for neighorn in get_neighorns(*coordinates) if neighorn in black_tiles)
+
+
+def will_be_black(black_tiles: set, coordinates: tuple) -> bool:
+    black_neighbors_number = count_black_neighorns(black_tiles, coordinates)
+    if coordinates in black_tiles:
+        return 0 < black_neighbors_number <= 2
+    else:
+        return black_neighbors_number == 2
+
+
+def solution_for_second_part(task_input: [str]) -> int:
+    all_black = get_black_tiles(task_input)
+
+    for _ in range(100):
+        new_all_black = set()
+        checked = set()
+
+        for black_tile in all_black:
+            for coordiantes in chain([black_tile], get_neighorns(*black_tile)):
+                if coordiantes in checked:
+                    continue
+
+                checked.add(coordiantes)
+                if will_be_black(all_black, coordiantes):
+                    new_all_black.add(coordiantes)
+
+        all_black = new_all_black
+
+    return len(all_black)
+
+
+assert solution_for_second_part(example_input) == 2208
+print("Solution for the second part:", solution_for_second_part(task_input))
