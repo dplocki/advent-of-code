8546945c334802ce76a8271e8b37af3a3f10b46b
Dawid Płocki <dawid.plocki@gmail.com>
2021-12-05 09:29:09 +0100

    Solution for the 5th task (second part) in 2021

--- 2021/2021_05.py
+++ 2021/2021_05.py
@@ -17,18 +17,30 @@ def parse(task_input: List[str]):
             yield int(groups[1]), int(groups[2]), int(groups[3]), int(groups[4])
 
 
+def mark_lines(points_map, points):
+    xs, ys, xe, ye = points
+
+    if xs == xe:
+        for i in range(min(ys, ye), max(ys, ye) + 1):
+            points_map[xs, i] += 1
+    elif ys == ye:
+        for i in range(min(xs, xe), max(xs, xe) + 1):
+            points_map[i, ys] += 1
+
+    return points_map
+
+
+def find_overlapping(task_input, mark_points):
+    point_map = defaultdict(int)
+
+    for points in parse(task_input):
+        point_map = mark_points(point_map, points)
+
+    return sum(1 for v in point_map.values() if v >= 2)
+
+
 def solution_for_first_part(task_input):
-    points = defaultdict(int)
-
-    for xs, ys, xe, ye in parse(task_input):
-        if xs == xe:
-            for i in range(min(ys, ye), max(ys, ye) + 1):
-                points[xs, i] += 1
-        elif ys == ye:
-            for i in range(min(xs, xe), max(xs, xe) + 1):
-                points[i, ys] += 1
-
-    return sum(1 for v in points.values() if v >= 2)
+    return find_overlapping(task_input, mark_lines)
 
 
 example_input = '''0,9 -> 5,9
@@ -48,3 +60,31 @@ assert solution_for_first_part(example_input) == 5
 # The input is taken from: https://adventofcode.com/2021/day/5/input
 task_input = list(load_input_file('input.05.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input):
+
+    def mark_digals(points_map, points):
+        xs, ys, xe, ye = points
+
+        if abs(xs - xe) == abs(ys - ye):
+            nxs, nxe = min(xs, xe), max(xs, xe)
+            nys, nye = (ys, ye) if nxs == xs else (ye, ys)
+
+            step = 1 if nys < nye else -1
+
+            for i in range(0, nxe - nxs + 1):
+                points_map[nxs + i, nys + step * i] += 1
+
+        return points_map
+
+    def mark_with_digals(points_map, points):
+        points_map = mark_digals(points_map, points)
+        return mark_lines(points_map, points)
+
+
+    return find_overlapping(task_input, mark_with_digals)
+
+
+assert solution_for_second_part(example_input) == 12
+print("Solution for the second part:", solution_for_second_part(task_input))
