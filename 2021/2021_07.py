dcc9d4ec01ab9beef63c6c64e3226a29c3e0f167
Dawid Płocki <dawid.plocki@gmail.com>
2021-12-07 07:00:16 +0100

    Solution for the 7th task (second part) in 2021

--- 2021/2021_07.py
+++ 2021/2021_07.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Counter
 
 
 def load_input_file(file_name: str) -> str:
@@ -6,25 +6,37 @@ def load_input_file(file_name: str) -> str:
         return file.read().strip()
 
 
-def parse(task_input: List[str]):
+def parse(task_input: str) -> list[int]:
     return map(int, task_input.split(','))
 
 
-def solution_for_first_part(task_input):
-    crabs = list(parse(task_input))
+def find_the_best_level(task_input: str, fuel_cost_calculation) -> int:
+    crabs = Counter(parse(task_input))
 
     lowest = min(crabs)
     higest = max(crabs)
 
     return min(
-        sum(abs(level - crab_level) for crab_level in crabs)
+        sum(fuel_cost_calculation(abs(level - crab_level)) * crabs_on_level for crab_level, crabs_on_level in crabs.items())
         for level in range(lowest, higest + 1))
 
 
-example_input = '''16,1,2,0,4,2,7,1,2,14'''
+def solution_for_first_part(task_input: str) -> int:
+    return find_the_best_level(task_input, lambda distance: distance)
+
+
+example_input = '16,1,2,0,4,2,7,1,2,14'
 
 assert solution_for_first_part(example_input) == 37
 
 # The input is taken from: https://adventofcode.com/2021/day/7/input
 task_input = load_input_file('input.07.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: str) -> int:
+    return find_the_best_level(task_input, lambda distance: distance * (distance + 1) // 2)
+
+
+assert solution_for_second_part(example_input) == 168
+print("Solution for the second part:", solution_for_second_part(task_input))
