92ae9e9c20fd0be2a65300531611624d8033e4be
Dawid Płocki <dawid.plocki@gmail.com>
2021-12-08 21:30:16 +0100

    Solution for the 8th task (second part) in 2021

--- 2021/2021_08.py
+++ 2021/2021_08.py
@@ -1,24 +1,29 @@
+from typing import Tuple
+
+
 def load_input_file(file_name: str) -> list[str]:
     with open(file_name) as file:
         yield from (line.strip() for line in file)
 
 
-def parse(task_input: list[str]):
+def parse(task_input: list[str]) -> Tuple[list[set], list[set]]:
     for line in task_input:
         tokens = line.split(' | ')
         yield list(map(set, tokens[0].split())), list(map(set, tokens[1].split()))
 
 
-def solution_for_first_part(task_input):
+def only_with_size(size: int) -> bool:
+    return lambda pattern: len(pattern) == size
 
-    def only_with_size(size):
-        return lambda pattern: len(pattern) == size
 
-    lines = list(parse(task_input))
-    
+def patterns_to_size_dictionary(patterns: list[set]) -> dict[int, list[set]]:
+    return {size:list(filter(only_with_size(size), patterns)) for size in range(2, 8)}
+
+
+def solution_for_first_part(task_input: Tuple[list[set], list[set]]) -> int:
     result = 0
-    for patterns, outputs in lines:
-        patterns_pre_size = {size:list(filter(only_with_size(size), patterns)) for size in range(2, 8)}
+    for patterns, outputs in parse(task_input):
+        patterns_pre_size = patterns_to_size_dictionary(patterns)
 
         result += sum(1
             for output in outputs
@@ -43,3 +48,47 @@ assert solution_for_first_part(example_input) == 26
 # The input is taken from: https://adventofcode.com/2021/day/8/input
 task_input = list(load_input_file('input.08.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+MULTIPLAYERS = (1000, 100, 10, 1)
+
+
+def solution_for_second_part(task_input: Tuple[list[set], list[set]]) -> int:
+    result = 0
+    for patterns, outputs in parse(task_input):
+        patterns_pre_size = patterns_to_size_dictionary(patterns)
+
+        digits = {}
+        digits[1] = patterns_pre_size[2][0]
+        digits[4] = patterns_pre_size[4][0]
+        digits[7] = patterns_pre_size[3][0]
+        digits[8] = patterns_pre_size[7][0]
+
+        digits[9] = next(filter(lambda p: digits[4].issubset(p), patterns_pre_size[6]))
+        patterns_pre_size[6].remove(digits[9])
+
+        digits[3] = next(filter(lambda p: digits[7].issubset(p), patterns_pre_size[5]))
+        patterns_pre_size[5].remove(digits[3])
+
+        zero_excluder = digits[4] - digits[1]
+        digits[0] = next(filter(lambda p: not zero_excluder.issubset(p), patterns_pre_size[6]))
+        patterns_pre_size[6].remove(digits[0])
+
+        digits[6] = patterns_pre_size[6][0]
+
+        five_excluder = list(digits[8] - digits[9])[0]
+        digits[2] = next(filter(lambda p: five_excluder in p, patterns_pre_size[5]))
+        patterns_pre_size[5].remove(digits[2])
+        digits[5] = patterns_pre_size[5][0]
+
+        result += sum(digit * multiplayer
+            for digit, multiplayer in zip(
+                    (next(digit for digit, combination in digits.items() if output == combination) for output in outputs),
+                    MULTIPLAYERS))
+    
+    return result
+
+
+assert solution_for_second_part(example_input) == 61229
+
+print("Solution for the second part:", solution_for_second_part(task_input))
