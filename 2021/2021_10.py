7cbf767cf2df476bdd4481e70ffda21c96e3ab93
Dawid Płocki <dawid.plocki@gmail.com>
2021-12-10 07:03:56 +0100

    Solution for the 10th task (second part) in 2021

--- 2021/2021_10.py
+++ 2021/2021_10.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 OPEN_TOKENS = '[({<'
 CLOSE_TOKENS = '])}>'
 
@@ -7,34 +10,36 @@ def load_input_file(file_name: str) -> list[str]:
         yield from (line.strip() for line in file)
 
 
-def find_opening(token):
+def find_opening(token: str) -> str:
     return OPEN_TOKENS[CLOSE_TOKENS.index(token)]
 
 
-def solution_for_first_part(task_input):
-    result = 0
-    for line in task_input:
-        open_chunks = []
+def find_closing(token: str) -> str:
+    return CLOSE_TOKENS[OPEN_TOKENS.index(token)]
+
+
+def parser(lines: list[str]) -> list[tuple[int, str]]:
+
+    def parse_line(line: str) -> tuple[int, str]:
+        opening_chunks = []
         for character in line:
             if character in OPEN_TOKENS:
-                open_chunks.append(character)
+                opening_chunks.append(character)
             elif character in CLOSE_TOKENS:
-                open_chunk = open_chunks.pop()
+                open_chunk = opening_chunks.pop()
                 if find_opening(character) != open_chunk:
-                    if character == ')':
-                        result += 3
-                    if character == ']':
-                        result += 57
-                    if character == '}':
-                        result += 1197
-                    if character == '>':
-                        result += 25137
+                    opening_chunks.append(character)
+                    return True, opening_chunks
 
-                    continue
-            else:
-                raise Exception('Unexcepted character')
+        return False, opening_chunks
 
-    return result
+
+    yield from (parse_line(line) for line in lines)
+
+
+def solution_for_first_part(task_input: list[str]) -> int:
+    score = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+    return sum(score[stack.pop()] for is_corrupted, stack in parser(task_input) if is_corrupted)
 
 
 example_input = '''[({(<(())[]>[[{[]{<()<>>
@@ -53,3 +58,17 @@ assert solution_for_first_part(example_input) == 26397
 # The input is taken from: https://adventofcode.com/2021/day/10/input
 task_input = list(load_input_file('input.10.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: list[str]) -> int:
+    score = { ')': 1, ']': 2, '}': 3, '>': 4 }
+    score_count = lambda result, current: result * 5 + score[find_closing(current)]
+
+    stack_from_incomplete = [reversed(stack) for is_corrupted, stack in parser(task_input) if not is_corrupted]
+    scores_per_lines = [reduce(score_count, stack, 0) for stack in stack_from_incomplete]
+
+    return sorted(scores_per_lines)[len(scores_per_lines) // 2]
+
+
+assert solution_for_second_part(example_input) == 288957
+print("Solution for the second part:", solution_for_second_part(task_input))
