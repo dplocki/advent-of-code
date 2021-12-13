9a98d9e7e342e0233aa3a9ef4b518a201255c153
Dawid Płocki <dawid.plocki@gmail.com>
2021-12-13 07:13:55 +0100

    Solution for the 13th task (second part) in 2021

--- 2021/2021_13.py
+++ 2021/2021_13.py
@@ -30,29 +30,34 @@ def apply_fold_horizontal(points: set, fold_y: int) -> set:
 def apply_fold_vertical(points: set, fold_x: int) -> set:
     result = set()
     for point in points:
-        if point[1] < fold_x:
+        if point[0] < fold_x:
             result.add(point)
 
-        if point[1] > fold_x:
-            result.add((fold_x - (point[1] - fold_x), point[1]))
+        if point[0] > fold_x:
+            result.add((fold_x - (point[0] - fold_x), point[1]))
 
     return result
 
 
-def solution_for_first_part(task_input) -> int:
+def fold_paper(task_input: str) -> list[set]:
     points, folds = parse(task_input)
 
-    how_fold, fold_line = folds[0]
-    if how_fold == 'x':
-        points = apply_fold_vertical(points, fold_line)
+    for how_fold, fold_line in folds:
+        if how_fold == 'x':
+            points = apply_fold_vertical(points, fold_line)
 
-    elif how_fold == 'y':
-        points = apply_fold_horizontal(points, fold_line)
+        elif how_fold == 'y':
+            points = apply_fold_horizontal(points, fold_line)
 
-    else:
-        raise Exception('Unknow folding way')
-    
-    return len(points)
+        else:
+            raise Exception('Unknow folding way')
+
+        yield points
+
+
+def solution_for_first_part(task_input: str) -> int:
+    one_fold = next(fold_paper(task_input))
+    return len(one_fold)
 
 
 example_input = '''6,10
@@ -77,8 +82,36 @@ example_input = '''6,10
 fold along y=7
 fold along x=5'''
 
-assert solution_for_first_part(example_input) == 17
+# assert solution_for_first_part(example_input) == 17
 
-# The input is taken from: https://adventofcode.com/2021/day/13/input
+# # The input is taken from: https://adventofcode.com/2021/day/13/input
 task_input = load_input_file('input.13.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input) -> int:
+
+    def visualization(points):
+        xs = [x for x, _ in points]
+        ys = [y for _, y in points]
+        max_x = max(xs)
+        max_y = max(ys)
+        min_x = min(xs)
+        min_y = min(ys)
+
+        for y in range(min_y, max_y + 1):
+            for x in range(min_x, max_x + 1):
+                print('#' if (x, y) in points else '.', end='')
+
+            print()
+
+
+    for fold in fold_paper(task_input):
+        pass
+
+    visualization(fold)
+
+
+solution_for_second_part(example_input)
+print("Solution for the second part:")
+solution_for_second_part(task_input)
