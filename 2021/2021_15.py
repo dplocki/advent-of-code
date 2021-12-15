87f6cae1cdb72c6c14e0a93c8f81b2dd222d8e25
Dawid Płocki <dawid.plocki@gmail.com>
2021-12-15 20:28:54 +0100

    Solution for the 15th task (second part) in 2021

--- 2021/2021_15.py
+++ 2021/2021_15.py
@@ -27,14 +27,8 @@ def get_neighborns(risk_map: dict[tuple[int, int], int], current: tuple[int, int
     yield from (neighbor for neighbor in get_four_neighbors(*current) if neighbor in risk_map)
 
 
-def solution_for_first_part(task_input: list[str]) -> int:
-    risk_map = {(x,y): risk for x, y, risk in parse(task_input)}
-    
-    end_x = max(x for x, _ in risk_map.keys())
-    end_y = max(y for _, y in risk_map.keys())
-
+def find_path_from_0_0(get_neighborns: callable, get_cost: callable, target: tuple[int, int]) -> int:
     start_point = (0, 0)
-    target = (end_x, end_y)
 
     frontier = PriorityQueue()
     frontier.put((0, start_point))
@@ -46,8 +40,8 @@ def solution_for_first_part(task_input: list[str]) -> int:
         if current == target:
             return cost_so_far[current]
 
-        for next in get_neighborns(risk_map, current):
-            new_cost = cost_so_far[current] + risk_map[next]
+        for next in get_neighborns(current):
+            new_cost = cost_so_far[current] + get_cost(next)
 
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
@@ -55,6 +49,15 @@ def solution_for_first_part(task_input: list[str]) -> int:
                 frontier.put((priority, next))
 
     raise Exception("no path found")
+
+
+def solution_for_first_part(task_input: list[str]) -> int:
+    risk_map = {(x,y): risk for x, y, risk in parse(task_input)}
+
+    end_x = max(x for x, _ in risk_map.keys())
+    end_y = max(y for _, y in risk_map.keys())
+
+    return find_path_from_0_0(lambda p: get_neighborns(risk_map, p), lambda p: risk_map[p], (end_x, end_y))
 
 
 example_input = '''1163751742
@@ -73,3 +76,32 @@ assert solution_for_first_part(example_input) == 40
 # The input is taken from: https://adventofcode.com/2021/day/15/input
 task_input = list(load_input_file('input.15.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: list[str]) -> int:
+    risk_map = {(x,y): risk for x, y, risk in parse(task_input)}
+
+    end_x = max(x for x, _ in risk_map.keys()) + 1
+    end_y = max(y for _, y in risk_map.keys()) + 1
+    len_x = end_x * 5
+    len_y = end_y * 5
+
+    def get_neighborns_on_bigger_map(point):
+        yield from (p for p in get_four_neighbors(*point) if p[0] >= 0 and p[0] < len_x and p[1] >= 0 and p[1] < len_y)
+
+    def calculate_cost(point):
+        if point[0] < end_x and point[1] < end_y:
+            return risk_map[point]
+
+        right_risk_factor = point[0] // end_x
+        up_risk_factor = point[1] // end_y
+
+        result = risk_map[point[0] % end_x, point[1] % end_y] + right_risk_factor + up_risk_factor
+        return (result - 1) % 9 + 1
+
+
+    return find_path_from_0_0(get_neighborns_on_bigger_map, calculate_cost, (len_x - 1, len_y - 1))
+
+
+assert solution_for_second_part(example_input) == 315
+print("Solution for the second part:", solution_for_second_part(task_input))
