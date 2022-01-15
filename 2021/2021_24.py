93bb6e7c8f8485b2f7f9e39184fb37f445c6c0a4
Dawid Płocki <dawid.plocki@gmail.com>
2022-01-15 09:14:50 +0100

    Solution for the 24th task (first part) in 2021

---
+++ 2021/2021_24.py
@@ -0,0 +1,86 @@
+import math
+import itertools
+
+
+def load_input_file(file_name: str) -> list[str]:
+    with open(file_name) as file:
+        yield from (line.strip() for line in file)
+
+
+def parse(task_input: list[str]):
+    for line in task_input:
+        tokens = line.split(' ')
+        
+        if len(tokens) == 3:
+            yield tokens[0], tokens[1], int(tokens[2]) if tokens[2] not in 'wxyz' else tokens[2]
+        else:
+            yield tokens[0], tokens[1], None
+
+
+def run_program(lines, input_gen, x=0, y=0, z=0):
+    """This method is not used in the solution, but it was usefull during analize the problem."""
+
+    def registe_or_value(registers, par):
+        if isinstance(par, int):
+            return par
+        
+        return registers[par]
+
+
+    registers = { 'w': 0, 'x': x, 'y': y, 'z': z }
+
+    for instruction, par1, par2 in lines:
+        if instruction == 'add':
+            registers[par1] = registers[par1] + registe_or_value(registers, par2)
+        elif instruction == 'inp':
+            registers[par1] = next(input_gen)
+        elif instruction == 'mul':
+            registers[par1] = registers[par1] * registe_or_value(registers, par2)
+        elif instruction == 'div':
+            a = math.trunc(registers[par1] // registe_or_value(registers, par2))
+            registers[par1] = a
+        elif instruction == 'mod':
+            registers[par1] = registers[par1] % registe_or_value(registers, par2)
+        elif instruction == 'eql':
+            registers[par1] = 1 if registers[par1] == registe_or_value(registers, par2) else 0
+
+    return registers
+
+
+def solution_for_first_part(task_input):
+    instructions = list(parse(task_input))
+
+    instructions_per_digit = []
+    for instruction in instructions:
+        if instruction[0] == 'inp':
+            current_digit = []
+            instructions_per_digit.append(current_digit)
+
+        current_digit.append(instruction)
+
+    variables = [(instructions[4][2], instructions[5][2], instructions[15][2]) for instructions in instructions_per_digit]
+
+    digit = None
+    result = None
+    for product in itertools.product(range(9, 0, -1), repeat=7):
+        digit_provider = iter(product)
+        result = []
+        z = 0
+        for p1, p2, p3 in variables:
+            if p2 < 0:
+                digit = z % 26 + p2
+                if digit > 9 or digit < 1:
+                    continue
+                z //= p1
+            else:
+                digit = next(digit_provider)
+                z = z // p1 * 26 + digit + p3
+
+            result.append(digit)
+
+        if z == 0:
+            return ''.join(map(str, result))
+
+# The input is taken from: https://adventofcode.com/2021/day/24/input
+task_input = list(load_input_file('input.24.txt'))
+print("Solution for the first part:", solution_for_first_part(task_input))
