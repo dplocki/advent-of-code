415866dc656e35f5fbbb5da7ea33aed4d0a555dd
Dawid Płocki <dawid.plocki@gmail.com>
2022-12-06 06:20:07 +0100

    Solution for the 6th task (second part) in 2022

--- 2022/2022_06.py
+++ 2022/2022_06.py
@@ -3,12 +3,14 @@ def load_input_file(file_name: str) -> str:
         return file.read().rstrip()
 
 
-def solution_for_first_part(task_input: str) -> int:
-    PACKAGE_SIZE = 4
+def solution(task_input: str, how_many_distinct: int) -> int:
+    for index in range(len(task_input) - how_many_distinct):
+        if len(set(task_input[index:index + how_many_distinct])) == how_many_distinct:
+            return index + how_many_distinct
 
-    for index in range(len(task_input) - PACKAGE_SIZE):
-        if len(set(task_input[index:index + PACKAGE_SIZE])) == PACKAGE_SIZE:
-            return index + PACKAGE_SIZE
+
+def solution_for_first_part(task_input: str) -> int:
+    return solution(task_input, 4)
 
 
 assert solution_for_first_part('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 7
@@ -17,7 +19,19 @@ assert solution_for_first_part('nppdvjthqldpwncqszvftbrmjlhg') == 6
 assert solution_for_first_part('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
 assert solution_for_first_part('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 11
 
-
 # The input is taken from: https://adventofcode.com/2022/day/6/input
 task_input = load_input_file('input.06.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: str) -> int:
+    return solution(task_input, 14)
+
+
+assert solution_for_second_part('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 19
+assert solution_for_second_part('bvwbjplbgvbhsrlpgdmjqwftvncz') == 23
+assert solution_for_second_part('nppdvjthqldpwncqszvftbrmjlhg') == 23
+assert solution_for_second_part('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 29
+assert solution_for_second_part('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 26
+
+print("Solution for the second part:", solution_for_second_part(task_input))
