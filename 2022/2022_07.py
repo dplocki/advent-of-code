879d3274463ef006d3b8b6f627ad7b62e9df2a92
Dawid Płocki <dawid.plocki@gmail.com>
2022-12-07 17:41:28 +0100

    Solution for the 8th task (second part) in 2022

--- 2022/2022_07.py
+++ 2022/2022_07.py
@@ -1,4 +1,7 @@
-from typing import Dict, Generator, List
+from typing import Dict, Generator, Iterable
+
+
+ROOT = '#'
 
 
 def load_input_file(file_name: str) -> Generator[str, None, None]:
@@ -6,8 +9,7 @@ def load_input_file(file_name: str) -> Generator[str, None, None]:
         yield from (line.rstrip() for line in file)
 
 
-def build_data_structure(task_input: List[str]) -> Dict[str, int]:
-    ROOT = '#'
+def build_data_structure(task_input: Iterable[str]) -> Dict[str, int]:
     data_structure = { ROOT: 0 }
     current_directory = [ ROOT ]
 
@@ -29,6 +31,7 @@ def build_data_structure(task_input: List[str]) -> Dict[str, int]:
             size = int(tokens[0])
 
             tmp_directory = ROOT
+            data_structure[tmp_directory] += size
             for dir in current_directory[1:]:
                 tmp_directory += '/' + dir
                 data_structure[tmp_directory] += size
@@ -36,7 +39,7 @@ def build_data_structure(task_input: List[str]) -> Dict[str, int]:
     return data_structure
 
 
-def solution_for_first_part(task_input) -> int:
+def solution_for_first_part(task_input: Iterable[str]) -> int:
     data_structure = build_data_structure(task_input)
 
     return sum(v for v in data_structure.values() if v < 100_000)
@@ -70,3 +73,14 @@ assert solution_for_first_part(example_input) == 95437
 # The input is taken from: https://adventofcode.com/2022/day/7/input
 task_input = list(load_input_file('input.07.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    data_structure = build_data_structure(task_input)
+    needed = 30_000_000 - (70_000_000 - data_structure[ROOT])
+    
+    return min(v for v in data_structure.values() if v > needed)
+
+
+assert solution_for_second_part(example_input) == 24933642
+print("Solution for the second part:", solution_for_second_part(task_input))
