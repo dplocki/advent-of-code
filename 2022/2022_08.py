1044b578011253a74ee09f22ebe842ad31bf5478
Dawid Płocki <dawid.plocki@gmail.com>
2022-12-08 16:02:31 +0100

    Solution for the 8th task (first part) in 2022

---
+++ 2022/2022_08.py
@@ -0,0 +1,66 @@
+from typing import Generator, Iterator
+
+
+def load_input_file(file_name: str) -> Generator[str, None, None]:
+    with open(file_name) as file:
+        yield from (line.rstrip() for line in file)
+
+
+def parse(task_input: Iterator[str]) -> Generator[str, None, None]:
+    for row_index, line in enumerate(task_input):
+        for column_index, character in enumerate(line):
+            yield row_index, column_index, int(character)
+
+
+def solution_for_first_part(task_input: Iterator[str]) -> int:
+    max_row, max_column, tree_map = 0, 0, {}
+    for row_index, column_index, tree_hight in parse(task_input):
+        tree_map[row_index, column_index] = tree_hight
+        max_column = max(column_index, max_column)
+        max_row = max(row_index, max_row)
+
+    result = set()
+    for row in range(1, max_row):
+        current_max = tree_map[row, 0]
+        for c in range(1, max_column):
+            current = tree_map[row, c]
+            if current > current_max:
+                result.add((row, c))
+                current_max = current
+
+        current_max = tree_map[row, max_row]
+        for c in range(max_column - 1, 0, -1):
+            current = tree_map[row, c]
+            if current > current_max:
+                result.add((row, c))
+                current_max = current
+
+    for column in range(1, max_column):
+        current_max = tree_map[0, column]
+        for r in range(1, max_row):
+            current = tree_map[r, column]
+            if current > current_max:
+                result.add((r, column))
+                current_max = current
+
+        current_max = tree_map[max_row, column]
+        for r in range(max_row - 1, 0, -1):
+            current = tree_map[r, column]
+            if current > current_max:
+                result.add((r, column))
+                current_max = current
+
+    return len(result) + 2 * max_row + 2 * max_column
+
+
+example_input = '''30373
+25512
+65332
+33549
+35390'''.splitlines()
+
+assert solution_for_first_part(example_input) == 21
+
+# The input is taken from: https://adventofcode.com/2022/day/8/input
+task_input = list(load_input_file('input.08.txt'))
+print("Solution for the first part:", solution_for_first_part(task_input))
