45fddc5a827185374fca38849eadfa7bd31d06fb
Dawid Płocki <dawid.plocki@gmail.com>
2022-12-08 16:12:25 +0100

    Solution for the 8th task (second part) in 2022

--- 2022/2022_08.py
+++ 2022/2022_08.py
@@ -1,4 +1,4 @@
-from typing import Generator, Iterator
+from typing import Dict, Generator, Iterator, Tuple
 
 
 def load_input_file(file_name: str) -> Generator[str, None, None]:
@@ -12,12 +12,18 @@ def parse(task_input: Iterator[str]) -> Generator[str, None, None]:
             yield row_index, column_index, int(character)
 
 
-def solution_for_first_part(task_input: Iterator[str]) -> int:
+def build_tree_map(task_input: Iterator[str]) -> Tuple[int, int, Dict[Tuple[int, int], int]]:
     max_row, max_column, tree_map = 0, 0, {}
     for row_index, column_index, tree_hight in parse(task_input):
         tree_map[row_index, column_index] = tree_hight
         max_column = max(column_index, max_column)
         max_row = max(row_index, max_row)
+
+    return max_row, max_column, tree_map
+
+
+def solution_for_first_part(task_input: Iterator[str]) -> int:
+    max_row, max_column, tree_map = build_tree_map(task_input)
 
     result = set()
     for row in range(1, max_row):
@@ -64,3 +70,34 @@ assert solution_for_first_part(example_input) == 21
 # The input is taken from: https://adventofcode.com/2022/day/8/input
 task_input = list(load_input_file('input.08.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterator[str]) -> int:
+    max_row, max_column, tree_map = build_tree_map(task_input)
+
+    def count_visible_trees(current, start, stop, step, take):
+        trees = 1
+        for coordinate in range(start, stop, step):
+            if take(coordinate) >= current:
+                break
+
+            trees += 1
+
+        return trees
+
+    result = {}
+    for row in range(1, max_row):
+        for column in range(1, max_column):
+            current_point = tree_map[row, column]
+
+            visible_trees_up = count_visible_trees(current_point, column - 1, 0, -1, lambda c: tree_map[row, c])
+            visible_trees_down = count_visible_trees(current_point, column + 1, max_column, 1, lambda c: tree_map[row, c])
+            visible_trees_left = count_visible_trees(current_point, row - 1, 0, -1, lambda r: tree_map[r, column])
+            visible_trees_right = count_visible_trees(current_point, row + 1, max_row, 1, lambda r: tree_map[r, column])
+
+            result[row, column] = visible_trees_up * visible_trees_down * visible_trees_left * visible_trees_right
+
+    return max(result.values())
+
+
+print("Solution for the second part:", solution_for_second_part(task_input))
