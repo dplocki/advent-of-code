217764abfb7f3b7af2f490253a78a6982daf3e88
Dawid Płocki <dawid.plocki@gmail.com>
2022-12-18 10:40:45 +0100

    Solution for the 18th task (second part) in 2022

--- 2022/2022_18.py
+++ 2022/2022_18.py
@@ -12,12 +12,11 @@ def parse(task_input: Iterable[str]):
         yield tuple(map(int, line.split(',')))
 
 
-def manhattan_distance(point1: Tuple[int, ...], point2: Tuple[int, ...]) -> int:
+def manhattan_distance(point1: Tuple[int, int, int], point2: Tuple[int, int, int]) -> int:
     return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1]) + abs(point1[2] - point2[2])
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
-    cubes = set(parse(task_input))
+def count_all_sides_of_cubes(cubes: Iterable[Tuple[int, int, int]]) -> int:
     result = len(cubes) * 6
 
     for c1, c2 in itertools.combinations(cubes, 2):
@@ -25,6 +24,10 @@ def solution_for_first_part(task_input: Iterable[str]) -> int:
             result -= 2
 
     return result
+
+
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+    return count_all_sides_of_cubes(list(parse(task_input)))
 
 
 example_input = '''2,2,2
@@ -45,3 +48,49 @@ assert solution_for_first_part(example_input) == 64
 # The input is taken from: https://adventofcode.com/2022/day/18/input
 task_input = list(load_input_file('input.18.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    cubes = set(parse(task_input))
+
+    xs = [x for x, _, _ in cubes]
+    ys = [y for _, y, _ in cubes]
+    zs = [z for _, _, z in cubes]
+    max_x = max(xs) + 1
+    max_y = max(ys) + 1
+    max_z = max(zs) + 1
+    min_x = min(xs) - 1
+    min_y = min(ys) - 1
+    min_z = min(zs) - 1
+    
+    internal_cubes = set()
+    for x in range(min_x, max_x):
+        for y in range(min_y, max_y):
+            for z in range(min_z, max_z):
+                if (x, y, z) not in cubes:
+                    internal_cubes.add((x, y, z))
+
+    todo = [(min_x + 1, min_y + 1, min_y + 1)]
+    while todo:
+        point = todo.pop()
+        if point not in internal_cubes:
+            continue
+
+        internal_cubes.remove(point)
+        todo.append((point[0] - 1, point[1], point[2]))
+        todo.append((point[0] + 1, point[1], point[2]))
+        todo.append((point[0], point[1] - 1, point[2]))
+        todo.append((point[0], point[1] + 1, point[2]))
+        todo.append((point[0], point[1], point[2] - 1))
+        todo.append((point[0], point[1], point[2] + 1))
+
+    result = count_all_sides_of_cubes(cubes)
+    for c, i in itertools.product(cubes, internal_cubes):
+        if manhattan_distance(c, i) == 1:
+            result -= 1
+
+    return result
+
+
+assert solution_for_second_part(example_input) == 58
+print("Solution for the second part:", solution_for_second_part(task_input))
