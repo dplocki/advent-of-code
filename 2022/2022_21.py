a268887449592460681572080216c2502c7b2206
Dawid Płocki <dawid.plocki@gmail.com>
2022-12-21 15:48:02 +0100

    Solution for the 21th task (second part) in 2022

--- 2022/2022_21.py
+++ 2022/2022_21.py
@@ -1,4 +1,4 @@
-from typing import Generator, Iterable
+from typing import Dict, Generator, Iterable, Tuple, Union
 
 
 def load_input_file(file_name: str) -> Generator[str, None, None]:
@@ -13,35 +13,38 @@ def parse(task_input: Iterable[str]):
         yield monkey_name, int(monkey_instructions[0]) if len(monkey_instructions) == 1 else tuple(monkey_instructions)
 
 
+def find_what_monkey_yells(monkeys_instructions: Dict[str, Union[int, Tuple[str, str, str]]], requested_monkey_name: str) -> int:
+    monkey_yells = monkeys_instructions.get(requested_monkey_name, None)
+
+    if isinstance(monkey_yells, int):
+        return monkey_yells
+    elif isinstance(monkey_yells, tuple):
+        first_monkey, operation, second_monkey = monkey_yells
+
+        first_monkey_yells = find_what_monkey_yells(monkeys_instructions, first_monkey)
+        second_monkey_yells = find_what_monkey_yells(monkeys_instructions, second_monkey)
+
+        if first_monkey_yells == None or second_monkey_yells == None:
+            return None
+
+        if operation == '+':
+            return first_monkey_yells + second_monkey_yells
+        elif operation == '*':
+            return first_monkey_yells * second_monkey_yells
+        elif operation == '-':
+            return first_monkey_yells - second_monkey_yells
+        elif operation == '/':
+            return first_monkey_yells // second_monkey_yells
+        else:
+            raise Exception('Unknown operation')
+    else:
+        return None
+
+
 def solution_for_first_part(task_input: Iterable[str]) -> int:
-    monkeys_instructions = list(parse(task_input))
-    result = {}
-    while True:
-        for monkey_name, monkey_yells in monkeys_instructions:
-            if monkey_name in result:
-                continue
+    monkeys_instructions = {monkey_name:monkey_yells for monkey_name, monkey_yells in parse(task_input)}
 
-            if isinstance(monkey_yells, tuple):
-                first_monkey, operation, second_monkey = monkey_yells
-                
-                if first_monkey in result and second_monkey in result:
-                    if operation == '+':
-                        result[monkey_name] = result[first_monkey] + result[second_monkey]
-                    elif operation == '*':
-                        result[monkey_name] = result[first_monkey] * result[second_monkey]
-                    elif operation == '-':
-                        result[monkey_name] = result[first_monkey] - result[second_monkey]
-                    elif operation == '/':
-                        result[monkey_name] = result[first_monkey] // result[second_monkey]
-                    else:
-                        raise Exception('Unknown operation')
-            elif isinstance(monkey_yells, int):
-                result[monkey_name] = monkey_yells
-            else:
-                raise Exception('Unknown monkey instruction input')
-
-        if 'root' in result:
-            return result['root']
+    return find_what_monkey_yells(monkeys_instructions, 'root')
 
 
 example_input = '''root: pppw + sjmn
@@ -65,3 +68,55 @@ assert solution_for_first_part(example_input) == 152
 # The input is taken from: https://adventofcode.com/2022/day/21/input
 task_input = list(load_input_file('input.21.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def find_value_for_human(monkeys_instructions: Dict[str, Union[int, Tuple[str, str, str]]], result_value: int, monkey_name: str) -> int:
+    if monkey_name == 'humn':
+        return result_value
+
+    monkeys_yells = monkeys_instructions[monkey_name]
+    first_monkey, operation, second_monkey = monkeys_yells
+    
+    first_monkey_value = find_what_monkey_yells(monkeys_instructions, first_monkey)
+    second_monkey_value = find_what_monkey_yells(monkeys_instructions, second_monkey)
+
+    if first_monkey_value == None:
+        if operation == '+':
+            result_value -= second_monkey_value
+        elif operation == '-':
+            result_value += second_monkey_value
+        elif operation == '*': 
+            result_value //= second_monkey_value
+        elif operation == '/':
+            result_value *= second_monkey_value
+
+        return find_value_for_human(monkeys_instructions, result_value, first_monkey)
+    elif second_monkey_value == None:
+        if operation == '+':
+            result_value -= first_monkey_value
+        elif operation == '-':
+            result_value = first_monkey_value - result_value
+        elif operation == '*': 
+            result_value //= first_monkey_value
+        elif operation == '/':
+            result_value = first_monkey_value // result_value
+
+        return find_value_for_human(monkeys_instructions, result_value, second_monkey)
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    monkeys_instructions = {monkey_name:monkey_yells for monkey_name, monkey_yells in parse(task_input) if monkey_name != 'humn'}
+
+    first_monkey_name, _, second_monkey_name = monkeys_instructions['root']
+    first_monkey_value = find_what_monkey_yells(monkeys_instructions, first_monkey_name)
+    second_monkey_value = find_what_monkey_yells(monkeys_instructions, second_monkey_name)
+
+    if first_monkey_value == None:
+        return find_value_for_human(monkeys_instructions, second_monkey_value, first_monkey_name)
+    elif second_monkey_value == None:
+        return find_value_for_human(monkeys_instructions, first_monkey_value, second_monkey_name)
+
+
+assert solution_for_second_part(example_input) == 301
+
+print("Solution for the second part:", solution_for_second_part(task_input))
