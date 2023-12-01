1fb9a02ea63e6c7426e760b44366700fb34ef1e7
Dawid Płocki <dawid.plocki@gmail.com>
2023-12-01 07:00:43 +0100

    Solution for the 1st task (second part) in 2023

--- 2023/2023_01.py
+++ 2023/2023_01.py
@@ -6,13 +6,12 @@ def load_input_file(file_name: str) -> Generator[str, None, None]:
         yield from (line.rstrip() for line in file)
 
 
+def find_first_and_last_digit(line: str) -> int:
+    digits = [c for c in line if c in '0123456789']
+    return int(digits[0] + digits[-1])
+
+
 def solution_for_first_part(task_input: Iterable[str]) -> int:
-
-    def find_first_and_last_digit(line: str) -> int:
-        digits = [c for c in line if c in '0123456789']
-        return int(digits[0] + digits[-1])
-
-
     return sum(find_first_and_last_digit(line) for line in task_input)
 
 
@@ -24,3 +23,26 @@ treb7uchet'''.splitlines()) == 142
 # The input is taken from: https://adventofcode.com/2023/day/1/input
 task_input = list(load_input_file('input.01.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+
+    def replace_word_digits(line: str) -> str:
+        for value_minus_one, word_digit in enumerate(['one','two','three','four','five','six','seven','eight','nine']):
+            line = line.replace(word_digit, word_digit[0] + str(value_minus_one + 1) + word_digit[-1])
+
+        return line
+
+
+    return sum(map(find_first_and_last_digit, map(replace_word_digits, task_input)))
+
+
+assert solution_for_second_part('''two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen'''.splitlines()) == 281
+
+print("Solution for the first part:", solution_for_second_part(task_input))
