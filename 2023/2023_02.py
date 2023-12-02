1ef3fae3cd821482a01d7dd3d36a202e0b1995ca
Dawid Płocki <dawid.plocki@gmail.com>
2023-12-02 06:51:41 +0100

    Solution for the 2sd task (second part) in 2023

--- 2023/2023_02.py
+++ 2023/2023_02.py
@@ -1,4 +1,4 @@
-from typing import Generator, Iterable
+from typing import Callable, Generator, Iterable
 
 
 def load_input_file(file_name: str) -> Generator[str, None, None]:
@@ -6,42 +6,44 @@ def load_input_file(file_name: str) -> Generator[str, None, None]:
         yield from (line.rstrip() for line in file)
 
 
-def game_fits(game: str) -> bool:
-    for s in game.split(';'):
+def parse(line: str) -> bool:
+    result = {}
 
-        cubes = s.split(',')
+    tokens = line.split(':')
+    result['id'] = int(tokens[0].split(' ')[1])
+    result['green'] = []
+    result['red'] = []
+    result['blue'] = []
 
-        for c in cubes:
+    for subsets in tokens[1].split(';'):
+        cubes_tokens = subsets.split(',')
+
+        for c in cubes_tokens:
             c = c.strip()
             n, name = c.split(' ')
             n = int(n)
 
-            if name == 'red' and n > 12:
-                return False
+            result[name].append(n)
+
+    return result
 
 
-            if name == 'green' and n > 13:
-                return False
+def internal_logic_for_first_part(game_object: dict):
+    # possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes
+    if any(e > 12 for e in game_object['red']) or \
+        any(e > 13 for e in game_object['green']) or \
+        any(e > 14 for e in game_object['blue']):
+        return 0
+
+    return game_object['id']
 
 
-            if name == 'blue' and n > 14:
-                return False
-
-    return True
+def solution(task_input: Iterable[str], internal_logic: Callable[[str], int]) -> int:
+    return sum(map(internal_logic, map(parse, task_input)))
 
 
 def solution_for_first_part(task_input: Iterable[str]) -> int:
-    result = 0
-
-    for line in task_input:
-        tokens = line.split(':')
-        game = tokens[1]
-        game_id = int(tokens[0].split(' ')[1])
-
-        if game_fits(game):
-            result += game_id
-
-    return result
+    return solution(task_input, internal_logic_for_first_part)
 
 
 example_input = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -55,3 +57,16 @@ assert solution_for_first_part(example_input) == 8
 # The input is taken from: https://adventofcode.com/2023/day/2/input
 task_input = list(load_input_file('input.02.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def internal_logic_for_second_part(game_object: dict) -> int:
+    return max(game_object['red']) * max(game_object['green']) * max(game_object['blue'])
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    return solution(task_input, internal_logic_for_second_part)
+
+
+assert solution_for_second_part(example_input) == 2286
+
+print("Solution for the second part:", solution_for_second_part(task_input))
