5ee9d416a6a2c76709c027f0e236ff5b9a379b72
Dawid Płocki <dawid.plocki@gmail.com>
2023-12-06 07:01:57 +0100

    Solution for the 6th task (second part) in 2023

--- 2023/2023_06.py
+++ 2023/2023_06.py
@@ -8,11 +8,11 @@ def load_input_file(file_name: str) -> str:
         return file.read().rstrip()
 
 
-def parse(task_input: str) -> Tuple[int, int]:
+def parse_for_first_task(task_input: str) -> Tuple[int, int]:
     return zip(*map(lambda line: tuple(map(int, line.split()[1:])), task_input.split('\n')))
 
 
-def find_how_many_wins(race_time, record_distance):
+def find_how_many_wins(race_time: int, record_distance: int) -> int:
     return sum(1 for _ in filter(
         lambda reached_distance: reached_distance > record_distance,
         map(
@@ -22,13 +22,29 @@ def find_how_many_wins(race_time, record_distance):
 
 
 def solution_for_first_part(task_input: str) -> int:
-    return reduce(mul, (find_how_many_wins(time, distance) for time, distance in parse(task_input)), 1)
+    return reduce(mul, (find_how_many_wins(time, distance) for time, distance in parse_for_first_task(task_input)), 1)
 
 
 example_input = '''Time:      7  15   30
 Distance:  9  40  200'''
 
-print(solution_for_first_part(example_input))
+assert solution_for_first_part(example_input) == 288
 # The input is taken from: https://adventofcode.com/2023/day/6/input
 task_input = load_input_file('input.06.txt')
-print("Solution for the first part:",  solution_for_first_part(task_input))
+print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def parse_for_second_task(task_input: str) -> Tuple[int, int]:
+    return tuple(
+        map(
+            lambda line: int(''.join(character for character in line if character.isnumeric())),
+            task_input.split('\n')
+        ))
+
+
+def solution_for_second_part(task_input: str) -> int:
+    return find_how_many_wins(*parse_for_second_task(task_input))
+
+
+assert solution_for_second_part(example_input) == 71503
+print("Solution for the second part:",  solution_for_second_part(task_input))
