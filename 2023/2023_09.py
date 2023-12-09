69808c8e643b23d3331356a2aaf5475a5a1d19e3
Dawid Płocki <dawid.plocki@gmail.com>
2023-12-09 15:31:59 +0100

    Solution for the 9th task (second part) in 2023

--- 2023/2023_09.py
+++ 2023/2023_09.py
@@ -1,5 +1,6 @@
-from typing import Generator, Iterable, Tuple
-import itertools
+from functools import reduce
+from typing import Callable, Generator, Iterable, Tuple
+from itertools import pairwise
 
 
 def load_input_file(file_name: str) -> Generator[str, None, None]:
@@ -12,19 +13,25 @@ def parse(task_input: Iterable[str]) -> Generator[Tuple, None, None]:
         yield tuple(map(int, line.split(' ')))
 
 
-def find_value_for(numbers: Tuple) -> int:
-    pyramid = [numbers]
+def build_pyramid(numbers: Tuple[int, ...]) -> Generator[Tuple, None, None]:
+    yield numbers
 
     while any(number != 0 for number in numbers):
-        new_numbers = tuple((b - a) for a, b in itertools.pairwise(numbers))
-        pyramid.append(new_numbers)
+        new_numbers = tuple((b - a) for a, b in pairwise(numbers))
+        yield new_numbers
         numbers = new_numbers
 
-    return sum(line[-1] for line in pyramid)
+
+def find_value_for_first_part(numbers: Tuple[int, ...]) -> int:
+    return sum(line[-1] for line in build_pyramid(numbers))
+
+
+def solution(task_input: Iterable[str], find_value_for: Callable[[str], int]) -> int:
+    return sum(find_value_for(line) for line in parse(task_input))
 
 
 def solution_for_first_part(task_input: Iterable[str]) -> int:
-    return sum(find_value_for(line) for line in parse(task_input))
+    return solution(task_input, find_value_for_first_part)
 
 
 example_input = '''0 3 6 9 12 15
@@ -36,3 +43,16 @@ assert solution_for_first_part(example_input) == 114
 # The input is taken from: https://adventofcode.com/2023/day/9/input
 task_input = list(load_input_file('input.09.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def find_value_for_second_part(numbers: Tuple[int, ...]) -> int:
+    pyramid = list(build_pyramid(numbers))
+    return reduce(lambda result, line: line[0] - result, pyramid[::-1], 0)
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    return solution(task_input, find_value_for_second_part)
+
+
+assert solution_for_second_part(example_input) == 2
+print("Solution for the second part:", solution_for_second_part(task_input))
