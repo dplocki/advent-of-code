ede2630ca162545a9d4c31dc53061a03cf496e7b
Dawid Płocki <dawid.plocki@gmail.com>
2023-12-16 07:32:45 +0100

    Solution for the 16th task (second part) in 2023

--- 2023/2023_16.py
+++ 2023/2023_16.py
@@ -14,10 +14,9 @@ def parse(task_input: Iterable[str]) -> Dict[Tuple[int, int], str]:
     }
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
-    mirrors_map = parse(task_input)
+def count_energized_titles(mirrors_map: Dict[Tuple[int, int], str], starting_point: Tuple[int, int, int, int]) -> int:
     visited = set()
-    rays = [(0, 0, 0, 1)]
+    rays = [starting_point]
 
     while rays:
         row_index, column_index, row_delta, column_delta = rays.pop()
@@ -68,6 +67,10 @@ def solution_for_first_part(task_input: Iterable[str]) -> int:
     return len(set((row_index, column_index) for row_index, column_index, _, _ in visited))
 
 
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+    return count_energized_titles(parse(task_input), (0, 0, 0, 1))
+
+
 example_input = '''.|...\....
 |.-.\.....
 .....|-...
@@ -84,3 +87,31 @@ assert solution_for_first_part(example_input) == 46
 # The input is taken from: https://adventofcode.com/2023/day/16/input
 task_input = list(load_input_file('input.16.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    mirrors_map = parse(task_input)
+    simple = next(iter(mirrors_map))
+    row_minium = simple[0]
+    row_maxim = simple[0]
+    column_minium = simple[1]
+    column_maxim = simple[1]
+
+    for row_index, column_index in mirrors_map.keys():
+        row_minium = min(row_minium, row_index)
+        row_maxim = max(row_maxim, row_index)
+        column_minium = min(column_minium, column_index)
+        column_maxim = max(column_maxim, column_index)
+
+    result = 0
+    for row_index in range(row_minium, row_maxim):
+        result = max(count_energized_titles(mirrors_map, (row_index, column_minium, 0, 1)), count_energized_titles(mirrors_map, (row_index, column_maxim, 0, -1)), result)
+
+    for column_index in range(column_minium, column_maxim):
+        result = max(count_energized_titles(mirrors_map, (row_minium, column_index, 1, 0)), count_energized_titles(mirrors_map, (row_minium, column_maxim, -1, 0)), result)
+
+    return result
+
+
+assert solution_for_second_part(example_input) == 51
+print("Solution for the second part:", solution_for_second_part(task_input))
