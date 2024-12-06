8f25f3afc9f75ffc7ba21629466d3c5c44a16516
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-06 22:00:58 +0100

    Solution for the 2sd task (first part) in 2024

---
+++ 2024/2024_02.py
@@ -0,0 +1,55 @@
+from typing import Generator, Iterable
+import itertools
+
+
+def load_input_file(file_name: str) -> Generator[str, None, None]:
+    with open(file_name) as file:
+        yield from (line.rstrip() for line in file)
+
+
+def parse(task_input: Iterable[str]):
+    for line in task_input:
+        yield list(map(int, line.split()))
+
+
+def is_safe(report: Iterable[int]) -> bool:
+    is_increasing = report[0] > report[1]
+
+    for a, b in itertools.pairwise(report):
+
+        if a == b:
+            return False
+
+        if is_increasing and a < b:
+            return False
+
+        if not is_increasing and a > b:
+            return False
+
+
+        difference = abs(a - b)
+
+        if difference < 1 or difference > 3:
+            return False
+
+    return True
+
+
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+    return sum(1 if is_safe(report) else 0
+        for report in parse(task_input)
+    )
+
+
+example_input = '''7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9'''.splitlines()
+
+solution_for_first_part(example_input) == 2
+
+# The input is taken from: https://adventofcode.com/2024/day/2/input
+task_input = list(load_input_file('input.02.txt'))
+print("Solution for the first part:", solution_for_first_part(task_input))
