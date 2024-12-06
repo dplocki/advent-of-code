e2bf205a467d3aa8a114a2cf629be79800ead1d0
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-06 22:09:04 +0100

    Solution for the 2sd task (second part) in 2024

--- 2024/2024_02.py
+++ 2024/2024_02.py
@@ -53,3 +53,23 @@ solution_for_first_part(example_input) == 2
 # The input is taken from: https://adventofcode.com/2024/day/2/input
 task_input = list(load_input_file('input.02.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def is_dampener_safe(report: Iterable[int]) -> bool:
+    for index in range(len(report)):
+        if is_safe(report):
+            return True
+
+        if is_safe(report[:index] + report[index + 1:]):
+            return True
+
+    return False
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    return sum(1 if is_dampener_safe(report) else 0
+        for report in parse(task_input)
+    )
+
+
+print("Solution for the second part:", solution_for_second_part(task_input))
