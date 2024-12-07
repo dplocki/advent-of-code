4ea3f92de9276387c1a12d278e7a50a37df32da9
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-07 10:38:33 +0100

    Solution for the 3rd task (second part) in 2024

--- 2024/2024_03.py
+++ 2024/2024_03.py
@@ -2,29 +2,54 @@ from typing import Generator, Iterable
 import re
 
 
-PATTERN = r"mul\(\d{1,3},\d{1,3}\)"
-
-
 def load_input_file(file_name: str) -> Generator[str, None, None]:
     with open(file_name) as file:
         yield from (line.rstrip() for line in file)
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
-    result = 0
+def load_instructions(task_input: Iterable[str]) -> Generator[str, None, None]:
+    PATTERN = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
 
     for line in task_input:
-        for multiple_operation in re.findall(PATTERN, line):
-            first_number, second_number = map(int, multiple_operation[len('mul('):-1].split(','))
-            result += first_number * second_number
-
-    return result
+        yield from re.findall(PATTERN, line)
 
 
-example_input = '''xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'''.splitlines()
+def do_multiplication(multiple_operation: str) -> int:
+    first_number, second_number = map(int, multiple_operation[len('mul('):-1].split(','))
+    return first_number * second_number
 
-assert solution_for_first_part(example_input) == 161
+
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+    return sum(
+        do_multiplication(multiple_operation)
+        for multiple_operation in load_instructions(task_input)
+        if multiple_operation.startswith('mul('))
+
+
+assert solution_for_first_part('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'.splitlines()) == 161
 
 # The input is taken from: https://adventofcode.com/2024/day/3/input
 task_input = list(load_input_file('input.03.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    result = 0
+    multiplication_enabled = True
+
+    for operation in load_instructions(task_input):
+        if operation == 'don\'t()':
+            multiplication_enabled = False
+        elif operation == 'do()':
+            multiplication_enabled = True
+        else:
+            if not multiplication_enabled:
+                continue
+
+            result += do_multiplication(operation)
+
+    return result
+
+
+assert solution_for_second_part("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".splitlines()) == 48
+print("Solution for the first part:", solution_for_second_part(task_input))
