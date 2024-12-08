f6797df9f76fdc96d2f11095bc56a3bd2976063b
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-08 07:17:07 +0100

    Solution for the 8th task (second part) in 2024

--- 2024/2024_08.py
+++ 2024/2024_08.py
@@ -1,4 +1,4 @@
-from typing import Generator, Iterable, Tuple
+from typing import Dict, Generator, Iterable, Tuple
 import itertools
 
 
@@ -11,7 +11,7 @@ def load_input_file(file_name: str) -> Generator[str, None, None]:
         yield from (line.rstrip() for line in file)
 
 
-def parse(task_input: Iterable[str]):
+def parse(task_input: Iterable[str]) -> Tuple[Dict[Tuple[int, int], int]]:
     rows = 0
     antennas_map = {}
 
@@ -30,28 +30,39 @@ def antinode_fits_to_size(size: Tuple[int, int], location: Tuple[int, int]) -> b
     return 0 <= location[ROW] < size[ROW] and 0 <= location[COLUMN] < size[COLUMN]
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
+def solution(antinode_generator: Generator[Tuple[int, int], None, None], task_input: Iterable[str]) -> int:
     antenna_map, size = parse(task_input)
     solutions = set()
-    is_matched = lambda location: antinode_fits_to_size(size, location)
 
     for antenna_a, antenna_b in itertools.combinations(antenna_map.keys(), r=2):
         if antenna_map[antenna_a] != antenna_map[antenna_b]:
             continue
 
+        for antinode in antinode_generator(size, antenna_a, antenna_b):
+            solutions.add(antinode)
+
+    return len(solutions)
+
+
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+
+    def antinode_generator(size, antenna_a, antenna_b):
+        is_matched = lambda location: antinode_fits_to_size(size, location)
+
         d_row = antenna_b[ROW] - antenna_a[ROW]
         d_column = antenna_b[COLUMN] - antenna_a[COLUMN]
 
-        antinode_a = antenna_b[ROW] + d_row, antenna_b[COLUMN] + d_column
-        antinode_b = antenna_a[ROW] - d_row, antenna_a[COLUMN] - d_column
+        antinode_a = antenna_a[ROW] - d_row, antenna_a[COLUMN] - d_column
+        antinode_b = antenna_b[ROW] + d_row, antenna_b[COLUMN] + d_column
 
         if is_matched(antinode_a):
-            solutions.add(antinode_a)
+            yield antinode_a
 
         if is_matched(antinode_b):
-            solutions.add(antinode_b)
+            yield antinode_b
 
-    return len(solutions)
+
+    return solution(antinode_generator, task_input)
 
 
 example_input = '''............
@@ -72,3 +83,43 @@ assert solution_for_first_part(example_input) == 14
 # The input is taken from: https://adventofcode.com/2024/day/8/input
 task_input = list(load_input_file('input.08.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+
+    def antinode_generator(size, antenna_a, antenna_b):
+        is_matched = lambda location: antinode_fits_to_size(size, location)
+
+        yield antenna_a
+        yield antenna_b
+
+        d_row = antenna_b[ROW] - antenna_a[ROW]
+        d_column = antenna_b[COLUMN] - antenna_a[COLUMN]
+
+        antinode_a = antenna_a[ROW] - d_row, antenna_a[COLUMN] - d_column
+        antinode_b = antenna_b[ROW] + d_row, antenna_b[COLUMN] + d_column
+
+        while True:
+            exit_a, exit_b = False, False
+
+            if is_matched(antinode_a):
+                yield antinode_a
+            else:
+                exit_a = True
+
+            if is_matched(antinode_b):
+                yield antinode_b
+            else:
+                exit_b = True
+
+            if exit_a and exit_b:
+                return
+
+            antinode_a = antinode_a[ROW] - d_row, antinode_a[COLUMN] - d_column
+            antinode_b = antinode_b[ROW] + d_row, antinode_b[COLUMN] + d_column
+
+    return solution(antinode_generator, task_input)
+
+
+assert solution_for_second_part(example_input) == 34
+print("Solution for the second part:", solution_for_second_part(task_input))
