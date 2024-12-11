1f8242544eab9324f1fc6bf45f7ac8b13ec31331
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-11 06:40:13 +0100

    Solution for the 11th task (second part) in 2024

--- 2024/2024_11.py
+++ 2024/2024_11.py
@@ -1,3 +1,4 @@
+import collections
 from typing import Iterable
 
 
@@ -6,30 +7,41 @@ def load_input_file(file_name: str) -> str:
         return file.read().rstrip()
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
-    stones = task_input.split()
+def solution(how_many_blinks: int, task_input: Iterable[str]) -> int:
+    stones = collections.Counter(task_input.split())
 
-    for _ in range(25):
-        new_stones = []
+    for _ in range(how_many_blinks):
+        new_stones = collections.Counter()
 
-        for stone in stones:
+        for stone, how_many in stones.items():
+            stone_number_length = len(stone)
+
             if stone == '0':
-                new_stones.append('1')
-            elif len(stone) % 2 == 0:
-                new_stones.append(str(int(stone[:len(stone) // 2])))
-                new_stones.append(str(int(stone[len(stone) // 2:])))
+                new_stones['1'] += how_many
+            elif stone_number_length % 2 == 0:
+                new_stones[str(int(stone[:stone_number_length // 2]))] += how_many
+                new_stones[str(int(stone[stone_number_length // 2:]))] += how_many
             else:
-                new_stones.append(str(int(stone) * 2024))
+                new_stones[str(int(stone) * 2024)] += how_many
 
         stones = new_stones
 
-    return len(stones)
+    return sum(stones.values())
 
 
-example_input = '125 17'
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+    return solution(25, task_input)
 
-assert solution_for_first_part(example_input) == 55312
+
+assert solution_for_first_part('125 17') == 55312
 
 # The input is taken from: https://adventofcode.com/2024/day/11/input
 task_input = load_input_file('input.11.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    return solution(75, task_input)
+
+
+print("Solution for the second part:", solution_for_second_part(task_input))
