f4e7b966f4bd47d0336c826f0a26d9b1212b1d05
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-12 13:17:14 +0100

    Solution for the 12th task (second part) in 2024

--- 2024/2024_12.py
+++ 2024/2024_12.py
@@ -1,4 +1,5 @@
-from typing import Dict, Generator, Iterable, Set, Tuple
+from operator import mul
+from typing import Callable, Dict, Generator, Iterable, Set, Tuple
 
 
 def load_input_file(file_name: str) -> str:
@@ -17,7 +18,7 @@ def parse(task_input: Iterable[str]) -> Dict[Tuple[int, int], str]:
         for column, character in enumerate(line)}
 
 
-def get_region(garden_map: Dict[Tuple[int, int], str], locations_measured: Set[Tuple[int, int]], location: Tuple[int, int]) -> Tuple[int, int]:
+def get_region_size_and_fence_length(garden_map: Dict[Tuple[int, int], str], locations_measured: Set[Tuple[int, int]], location: Tuple[int, int]) -> Tuple[int, int]:
     region = garden_map[location]
     size = 0
     fence = 0
@@ -42,19 +43,18 @@ def get_region(garden_map: Dict[Tuple[int, int], str], locations_measured: Set[T
     return size, fence
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
+def solution(get_region_price: Callable[[Dict[Tuple[int, int], str], Set[Tuple[int, int]], Tuple[int, int]], Tuple[int, int]], task_input: Iterable[str]) -> int:
     garden_map = parse(task_input)
     locations_measured = set()
-    result = 0
 
-    for location in garden_map:
-        if location in locations_measured:
-            continue
+    return sum(
+        mul(*get_region_price(garden_map, locations_measured, location))
+        for location in garden_map
+        if location not in locations_measured)
 
-        size, fence = get_region(garden_map, locations_measured, location)
-        result += size * fence
 
-    return result
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+    return solution(get_region_size_and_fence_length, task_input)
 
 
 example_input = '''RRRRIICCFF
@@ -73,3 +73,81 @@ assert solution_for_first_part(example_input) == 1930
 # The input is taken from: https://adventofcode.com/2024/day/12/input
 task_input = list(load_input_file('input.12.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def get_region_size_and_sides(garden_map: Dict[Tuple[int, int], str], locations_measured: Set[Tuple[int, int]], location: Tuple[int, int]) -> Tuple[int, int]:
+    region = garden_map[location]
+    to_check = [location]
+    size = 0
+    sides = 0
+
+    while to_check:
+        current = to_check.pop()
+        if garden_map[current] != region:
+            continue
+
+        if current in locations_measured:
+            continue
+
+        locations_measured.add(current)
+
+        size += 1
+
+        if garden_map.get((current[0] - 1, current[1]), None) == region and garden_map.get((current[0], current[1] - 1), None) == region and garden_map.get((current[0] - 1, current[1] - 1), None) != region:
+            sides += 1
+
+        if garden_map.get((current[0] - 1, current[1]), None) != region and garden_map.get((current[0], current[1] - 1), None) != region:
+            sides += 1
+
+        if garden_map.get((current[0] - 1, current[1]), None) == region and garden_map.get((current[0], current[1] + 1), None) == region and garden_map.get((current[0] - 1, current[1] + 1), None) != region:
+            sides += 1
+
+        if garden_map.get((current[0] - 1, current[1]), None) != region and garden_map.get((current[0], current[1] + 1), None) != region:
+            sides += 1
+
+        if garden_map.get((current[0] + 1, current[1]), None) == region and garden_map.get((current[0], current[1] - 1), None) == region and garden_map.get((current[0] + 1, current[1] - 1), None) != region:
+            sides += 1
+
+        if garden_map.get((current[0] + 1, current[1]), None) != region and garden_map.get((current[0], current[1] - 1), None) != region:
+            sides += 1
+
+        if garden_map.get((current[0] + 1, current[1]), None) == region and garden_map.get((current[0], current[1] + 1), None) == region and garden_map.get((current[0] + 1, current[1] + 1), None) != region:
+            sides += 1
+
+        if garden_map.get((current[0] + 1, current[1]), None) != region and garden_map.get((current[0], current[1] + 1), None) != region:
+            sides += 1
+
+        for neighbor in ((0, -1), (1, 0), (-1, 0), (0, 1)):
+            new_location = (current[0] + neighbor[0], current[1] + neighbor[1])
+            value = garden_map.get(new_location, None)
+            if value != None:
+                to_check.append(new_location)
+
+    return size, sides
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    return solution(get_region_size_and_sides, task_input)
+
+
+assert solution_for_second_part(example_input) == 1206
+
+assert solution_for_second_part('''AAAA
+BBCD
+BBCC
+EEEC'''.splitlines()) == 80
+
+assert solution_for_second_part('''EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE'''.splitlines()) == 236
+
+assert solution_for_second_part('''AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA'''.splitlines()) == 368
+
+print("Solution for the second part:", solution_for_second_part(task_input))
