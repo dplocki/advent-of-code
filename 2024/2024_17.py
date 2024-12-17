8a3084eadf7276e48942fc3a634602abdf4c9a20
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-17 22:44:14 +0100

    Solution for the 17th task (second part) in 2024

--- 2024/2024_17.py
+++ 2024/2024_17.py
@@ -1,5 +1,6 @@
+from typing import Dict, Tuple
+from itertools import count
 import re
-from typing import Dict, Iterable, Tuple
 
 
 def load_input_file(file_name: str) -> str:
@@ -7,7 +8,7 @@ def load_input_file(file_name: str) -> str:
         return file.read().rstrip()
 
 
-def parse(task_input: Iterable[str]) -> Tuple[Dict[str, int], Tuple[int]]:
+def parse(task_input: str) -> Tuple[Dict[str, int], Tuple[int]]:
     PATTERN = 'Register ([A-Z]): (\d+)'
     raw_registers, program_line = task_input.split('\n\n')
 
@@ -61,7 +62,7 @@ def run_program(registers: Dict[str, int], instructions: Tuple[int, ...]) -> Tup
     return ','.join(map(str, output)), registers
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
+def solution_for_first_part(task_input: str) -> int:
     registers, instructions = parse(task_input)
     return run_program(registers, instructions)[0]
 
@@ -77,3 +78,59 @@ assert solution_for_first_part(example_input) == '4,6,3,5,6,3,5,2,1,0'
 # The input is taken from: https://adventofcode.com/2024/day/17/input
 task_input = load_input_file('input.17.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+example_input = '''Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0'''
+
+
+def solution_for_second_part(task_input: str) -> int:
+    # Brute-force attempt, too slow for final answer
+
+    registers, instructions = parse(task_input)
+    requested_output = ','.join(map(str, instructions))
+
+    for a_value in count(8 ** (len(instructions) - 1)):
+        tmp_registers = {
+            'A': a_value,
+            'B': registers['B'],
+            'C': registers['C'],
+        }
+
+        output = run_program(tmp_registers, instructions)[0]
+        if requested_output == output:
+            return a_value
+
+
+assert solution_for_second_part(example_input) == 117440
+
+
+def calculate_output(current_value_a: int, digit: int) -> int:
+    A = current_value_a * 8 + digit
+
+    #
+    # Place translated to Python program here
+    #
+
+    return B % 8
+
+
+def solution_for_second_part(task_input: str):
+    _, instructions = parse(task_input)
+    results = [ 0 ]
+
+    for program_code in reversed(instructions):
+        results = [
+            (current_value_a * 8 + digit)
+            for current_value_a in results
+            for digit in range(8)
+            if calculate_output(current_value_a, digit) == program_code
+        ]
+
+    return min(results)
+
+
+print("Solution for the second part:", solution_for_second_part(task_input))
