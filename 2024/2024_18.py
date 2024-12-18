bba283be1a899417bae4edd5cece61b492eb4aa6
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-18 18:10:31 +0100

    Solution for the 18th task (second part) in 2024

--- 2024/2024_18.py
+++ 2024/2024_18.py
@@ -1,5 +1,5 @@
 import heapq
-from typing import Generator, Iterable, Tuple
+from typing import Dict, Generator, Iterable, Tuple
 
 
 NEIGHBORS = ((0, -1), (1, 0), (-1, 0), (0, 1))
@@ -19,25 +19,14 @@ def heuristic(point_from: Tuple[int, int], point_to: Tuple[int, int]) -> int:
     return abs(point_from[0] - point_to[0]) + abs(point_from[1] - point_to[1])
 
 
-def solution_for_first_part(task_input: Iterable[str], memory_map_size: int, first_elements_count: int) -> int:
-    memory_map = {
-        (x, y): '.'
-        for x in range(memory_map_size)
-        for y in range(memory_map_size)
-    }
-
-    for (x, y), _ in zip(parse(task_input), range(first_elements_count)):
-        memory_map[x, y] = '#'
-
-    start = (0, 0)
+def find_path(memory_map: Dict[Tuple[int, int], str], start: Tuple[int, int], end: Tuple[int, int]) -> int:
     to_check = []
     visited = { start: 0 }
-    end = (memory_map_size - 1, memory_map_size - 1)
 
     heapq.heappush(to_check, (0, 0, start))
 
     while to_check:
-        priority, steps_count, current =  heapq.heappop(to_check)
+        priority, steps_count, current = heapq.heappop(to_check)
         if current == end:
             return steps_count
 
@@ -53,6 +42,22 @@ def solution_for_first_part(task_input: Iterable[str], memory_map_size: int, fir
                 heapq.heappush(to_check, (priority, next_point_steps_count, next_point))
 
     return None
+
+
+def solution_for_first_part(task_input: Iterable[str], memory_map_size: int, first_elements_count: int) -> int:
+    memory_map = {
+        (x, y): '.'
+        for x in range(memory_map_size)
+        for y in range(memory_map_size)
+    }
+
+    for (x, y), _ in zip(parse(task_input), range(first_elements_count)):
+        memory_map[x, y] = '#'
+
+    start = (0, 0)
+    end = (memory_map_size - 1, memory_map_size - 1)
+
+    return find_path(memory_map, start, end)
 
 
 example_input = '''5,4
@@ -86,3 +91,31 @@ assert solution_for_first_part(example_input, 7, 12) == 22
 # The input is taken from: https://adventofcode.com/2024/day/18/input
 task_input = list(load_input_file('input.18.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input, 71, 1024))
+
+
+def solution_for_second_part(task_input: Iterable[str], memory_map_size: int, first_elements_count: int) -> int:
+    memory_map = {
+        (x, y): '.'
+        for x in range(memory_map_size)
+        for y in range(memory_map_size)
+    }
+
+    broken_bytes_generator = parse(task_input)
+
+    for (x, y), _ in zip(broken_bytes_generator, range(first_elements_count)):
+        memory_map[x, y] = '#'
+
+    start = (0, 0)
+    end = (memory_map_size - 1, memory_map_size - 1)
+
+    for (x, y) in broken_bytes_generator:
+        memory_map[x, y] = '#'
+
+        if find_path(memory_map, start, end) == None:
+            return f'{x},{y}'
+
+    raise Exception('No solution')
+
+
+assert solution_for_second_part(example_input, 7, 12) == '6,1'
+print("Solution for the second part:", solution_for_second_part(task_input, 71, 1024))
