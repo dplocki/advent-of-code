6bfe905edc1f637ea89ba987897587b7e996842d
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-19 06:50:33 +0100

    Solution for the 19th task (second part) in 2024

--- 2024/2024_19.py
+++ 2024/2024_19.py
@@ -13,7 +13,7 @@ def parse(task_input: Iterable[str]) -> Tuple[Iterable[str], Iterable[str]]:
     return tuple(token for token in raw_towels.split(', ')), tuple(line for line in raw_designs.splitlines())
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
+def solution(task_input: Iterable[str]) -> int:
     towels, designs = parse(task_input)
 
     @lru_cache
@@ -25,8 +25,14 @@ def solution_for_first_part(task_input: Iterable[str]) -> int:
             for towel in towels
             if target.startswith(towel))
 
+    return {
+        design:count_design_realizations(design)
+        for design in designs
+    }
 
-    return sum(count_design_realizations(design) > 0 for design in designs)
+
+def solution_for_first_part(task_input: Iterable[str]) -> int:
+    return sum(1 for value in solution(task_input).values() if value > 0)
 
 
 example_input = '''r, wr, b, g, bwu, rb, gb, br
@@ -45,3 +51,11 @@ assert solution_for_first_part(example_input) == 6
 # The input is taken from: https://adventofcode.com/2024/day/19/input
 task_input = load_input_file('input.19.txt')
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    return sum(solution(task_input).values())
+
+
+assert solution_for_second_part(example_input) == 16
+print("Solution for the second part:", solution_for_second_part(task_input))
