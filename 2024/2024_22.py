191fb6fd89ee7d4f9157a7f404d299b56d3b00e6
Dawid Płocki <dawid.plocki@gmail.com>
2024-12-22 08:20:24 +0100

    Solution for the 22th task (second part) in 2024

--- 2024/2024_22.py
+++ 2024/2024_22.py
@@ -1,4 +1,19 @@
 from typing import Generator, Iterable
+from more_itertools import last
+import collections
+import itertools
+
+
+def sliding_window(iterable, n):
+    """Collect data into overlapping fixed-length chunks or blocks.
+    My current version of the more_itertools doesn't provide that
+    Taken from: https://docs.python.org/3/library/itertools.html"""
+    # sliding_window('ABCDEFG', 4) → ABCD BCDE CDEF DEFG
+    iterator = iter(iterable)
+    window = collections.deque(itertools.islice(iterator, n - 1), maxlen=n)
+    for x in iterator:
+        window.append(x)
+        yield tuple(window)
 
 
 def load_input_file(file_name: str) -> Generator[str, None, None]:
@@ -19,18 +34,18 @@ def prune(secret: int) -> int:
     return secret % 16777216
 
 
+def generate_secret(secret: int) -> Generator[int, None, None]:
+    for _ in range(2000):
+        secret = prune(mix(secret, secret * 64))
+        secret = prune(mix(secret, secret // 32))
+        secret = prune(mix(secret, secret * 2048))
+        yield secret
+
+
 def solution_for_first_part(task_input: Iterable[str]) -> int:
-    result = 0
-
-    for secret in parse(task_input):
-        for _ in range(2000):
-            secret = prune(mix(secret, secret * 64))
-            secret = prune(mix(secret, secret // 32))
-            secret = prune(mix(secret, secret * 2048))
-
-        result += secret
-
-    return result
+    return sum(
+        last(generate_secret(secret))
+        for secret in parse(task_input))
 
 
 assert mix(42, 15) == 37
@@ -44,3 +59,36 @@ assert solution_for_first_part('''1
 # The input is taken from: https://adventofcode.com/2024/day/22/input
 task_input = list(load_input_file('input.22.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def generate_price_difference(secret: int) -> Generator[int, None, None]:
+    previous_price = 0
+
+    for secret in generate_secret(secret):
+        bananas = secret % 10
+        yield bananas - previous_price, bananas
+        previous_price = bananas
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    result = {}
+
+    for secret in parse(task_input):
+        sequence_values = {}
+        for sequence in sliding_window(generate_price_difference(secret), 4):
+            change = ','.join(str(s[0]) for s in sequence)
+            if change not in sequence_values:
+                sequence_values[','.join(str(s[0]) for s in sequence)] = sequence[-1][1]
+
+        for k, v in sequence_values.items():
+            result[k] = result.get(k, 0) + v
+
+    return max(result.values())
+
+
+assert solution_for_second_part('''1
+2
+3
+2024'''.splitlines()) == 23
+
+print("Solution for the second part:", solution_for_second_part(task_input))
