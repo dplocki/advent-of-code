8e01de85a7e4459312c02c20a9aac76300f874e8
Dawid Płocki <dawid.plocki@gmail.com>
2025-12-01 06:40:22 +0100

    Solution for the 1st task (second part) in 2025

--- 2025/2025_01.py
+++ 2025/2025_01.py
@@ -11,15 +11,15 @@ def parse(task_input: Iterable[str]) -> Generator[Tuple[str, int], None, None]:
         yield line[0], int(line[1:])
 
 
-def solution_for_first_part(task_input: Iterable[str]) -> int:
+def solution_for_first_part(task_input: Iterable[Tuple[str, int]]) -> int:
     dial = 50
     result = 0
 
-    for w, v in parse(task_input):
-        if w == 'R':
-            dial += v
-        elif w == 'L':
-            dial -= v
+    for way, value in parse(task_input):
+        if way == 'R':
+            dial += value
+        elif way == 'L':
+            dial -= value
 
         dial = dial % 100
 
@@ -45,3 +45,24 @@ assert solution_for_first_part(example_input) == 3
 # The input is taken from: https://adventofcode.com/2025/day/1/input
 task_input = list(load_input_file('input.01.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[Tuple[str, int]]) -> int:
+    dial = 50
+    result = 0
+
+    for way, value in parse(task_input):
+        way = 1 if way == 'R' else - 1
+
+        for _ in range(value):
+            dial += way
+            dial %= 100
+
+            if dial == 0:
+                result += 1
+
+    return result
+
+
+assert solution_for_second_part(example_input) == 6
+print("Solution for the second part:", solution_for_second_part(task_input))
