cbf1e81a58625df172a32bce22186b856bb7c01c
Dawid Płocki <dawid.plocki@gmail.com>
2025-12-03 17:42:19 +0100

    Solution for the 3rd task (second part) in 2025

--- 2025/2025_03.py
+++ 2025/2025_03.py
@@ -32,3 +32,37 @@ assert solution_for_first_part(example_input) == 357
 # The input is taken from: https://adventofcode.com/2025/day/3/input
 task_input = list(load_input_file('input.03.txt'))
 print("Solution for the first part:", solution_for_first_part(task_input))
+
+
+def solution_for_second_part(task_input: Iterable[str]) -> int:
+    MAX_BATTERIES = 12
+
+    def internal(batteries, index, result, digits):
+        if digits == MAX_BATTERIES:
+            yield result
+            return
+
+        batteries_len = len(batteries)
+        the_largest_digit = 0
+
+        for i in range(index, batteries_len):
+            if i + (MAX_BATTERIES - digits - 1) >= batteries_len:
+                return
+
+            if batteries[i] <= the_largest_digit:
+                continue
+
+            the_largest_digit = batteries[i]
+            result *= 10
+            result += the_largest_digit
+            yield from internal(batteries, i + 1, result, digits + 1)
+            result //= 10
+
+    return sum(
+        max(internal(batteries, 0, 0, 0))
+        for batteries in parse(task_input)
+    )
+
+
+assert solution_for_second_part(example_input) == 3121910778619
+print("Solution for the second part:", solution_for_second_part(task_input))
